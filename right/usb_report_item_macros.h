ee484d6b47a886f2fbab5d82642adb621036131d
László Monda <laci@monda.hu>
2016-03-02 16:11:30 +0100

    Feature the name of the relevant usage pages in usage names.

--- right/usb_report_item_macros.h
+++ right/usb_report_item_macros.h
@@ -10,18 +10,20 @@
 
     #define HID_RI_USAGE_PAGE_GENERIC_DESKTOP 0x01
     #define HID_RI_USAGE_PAGE_LEDS            0x08
-    #define HID_RI_USAGE_PAGE_MOUSE_BUTTONS   0x09
+    #define HID_RI_USAGE_PAGE_BUTTONS         0x09
     #define HID_RI_USAGE_PAGE_CONSUMER        0x0C
 
-    #define HID_RI_USAGE_POINTER               0x01
-    #define HID_RI_USAGE_MOUSE                 0x02
-    #define HID_RI_USAGE_JOYSTICK              0x04
-    #define HID_RI_USAGE_KEYBOARD              0x06
-    #define HID_RI_USAGE_X                     0x30
-    #define HID_RI_USAGE_Y                     0x31
-    #define HID_RI_USAGE_WHEEL                 0x38
-    #define HID_RI_USAGE_RESOLUTION_MULTIPLIER 0x48
-    #define HID_RI_USAGE_AC_PAN                0x0238
+    #define HID_RI_USAGE_GENERIC_DESKTOP_POINTER               0x01
+    #define HID_RI_USAGE_GENERIC_DESKTOP_MOUSE                 0x02
+    #define HID_RI_USAGE_GENERIC_DESKTOP_JOYSTICK              0x04
+    #define HID_RI_USAGE_GENERIC_DESKTOP_GAMEPAD               0x05
+    #define HID_RI_USAGE_GENERIC_DESKTOP_KEYBOARD              0x06
+    #define HID_RI_USAGE_GENERIC_DESKTOP_X                     0x30
+    #define HID_RI_USAGE_GENERIC_DESKTOP_Y                     0x31
+    #define HID_RI_USAGE_GENERIC_DESKTOP_WHEEL                 0x38
+    #define HID_RI_USAGE_GENERIC_DESKTOP_RESOLUTION_MULTIPLIER 0x48
+
+    #define HID_RI_USAGE_CONSUMER_AC_PAN 0x0238
 
     #define HID_RI_COLLECTION_PHYSICAL    0x00
     #define HID_RI_COLLECTION_APPLICATION 0x01
