d1ac7ba89270ae4abb46bfe0524398fa44e4689f
László Monda <laci@monda.hu>
2016-03-03 16:56:11 +0100

    Deduplicate macros.

--- right/usb_descriptor_configuration.h
+++ right/usb_descriptor_configuration.h
@@ -9,29 +9,8 @@
 
 // Macros:
 
-    #define USB_DEVICE_CLASS (0x00U)
-    #define USB_DEVICE_SUBCLASS (0x00U)
-    #define USB_DEVICE_PROTOCOL (0x00U)
-
-    #define USB_DEVICE_SPECIFICATION_VERSION (0x0200U)
-    #define USB_HID_VERSION (0x0100U)
-
-    #define USB_DEVICE_VENDOR_ID (0x15A2U)
-    #define USB_DEVICE_PRODUCT_ID (0x007EU)
-    #define USB_DEVICE_RELEASE_NUMBER (0x0101U)
-
     #define USB_COMPOSITE_CONFIGURATION_INDEX (1U)
-    #define USB_DEVICE_CONFIGURATION_COUNT (1U)
-    #define USB_REPORT_DESCRIPTOR_COUNT_PER_HID_DEVICE (1U)
-    #define USB_DEVICE_MAX_POWER (50U) // Expressed in 2mA units
-    #define USB_COMPOSITE_INTERFACE_COUNT (USB_KEYBOARD_INTERFACE_COUNT + \
-                                           USB_MOUSE_INTERFACE_COUNT + \
-                                           USB_GENERIC_HID_INTERFACE_COUNT)
-
     #define USB_LANGUAGE_ID_UNITED_STATES (0x0409U)
-    #define USB_HID_COUNTRY_CODE_NOT_SUPPORTED (0x00U)
-    #define USB_INTERFACE_ALTERNATE_SETTING_NONE (0x00U)
-    #define USB_STRING_DESCRIPTOR_NONE             0U
 
     // Descriptor lengths
 
