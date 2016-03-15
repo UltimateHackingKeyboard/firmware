4dfd710a65bbea85debcd9950c16cef4ed4d6869
László Monda <laci@monda.hu>
2016-03-15 18:14:43 +0100

    Add module name.

--- right/module.h
+++ right/module.h
@@ -17,17 +17,19 @@
 
     #define MODULE_REQUEST_GET_PROTOCOL_VERSION 0
     #define MODULE_REQUEST_GET_MODULE_ID        1
-    #define MODULE_REQUEST_GET_FEATURES         2
-    #define MODULE_REQUEST_GET_FACTORY_LAYER    3
-    #define MODULE_REQUEST_GET_POINTER_INFO     4
-    #define MODULE_REQUEST_GET_STATE            5
-    #define MODULE_REQUEST_GET_GRAPHICS         6
+    #define MODULE_REQUEST_GET_MODULE_NAME      2
+    #define MODULE_REQUEST_GET_FEATURES         3
+    #define MODULE_REQUEST_GET_FACTORY_LAYER    4
+    #define MODULE_REQUEST_GET_POINTER_INFO     5
+    #define MODULE_REQUEST_GET_STATE            6
+    #define MODULE_REQUEST_GET_GRAPHICS         7
 
     #define MODULE_STATUS_ATTACHED MODULE_REQUEST_GET_STATE
 
     #define POINTER_ROLE_MOVE   0
     #define POINTER_ROLE_SCROLL 1
 
+    #define MAX_MODULE_NAME_LENGTH       64
     #define MAX_KEY_COUNT_PER_MODULE     64
     #define MAX_POINTER_COUNT_PER_MODULE 2
 
@@ -43,6 +45,7 @@
         uint8_t enumerationState;
         uint8_t enumerationSubstate;
         uint8_t moduleId;
+        char moduleName[MAX_MODULE_NAME_LENGTH];
         uint8_t pointerCount;
         uint8_t keyCount;
         uint8_t keyStates[MAX_KEY_COUNT_PER_MODULE];
