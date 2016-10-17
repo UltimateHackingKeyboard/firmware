cb7c4d68dd597febeea54131797d42dee10c53e1
László Monda <laci@monda.hu>
2016-10-17 22:54:06 +0200

    Fix indentation.

--- right/src/reset_button.h
+++ right/src/reset_button.h
@@ -7,11 +7,11 @@
 
 // Macros:
 
-    #define RESET_BUTTON_GPIO GPIOB
-    #define RESET_BUTTON_PORT PORTB
-    #define RESET_BUTTON_CLOCK kCLOCK_PortB
-    #define RESET_BUTTON_PIN 1
-    #define RESET_BUTTON_IRQ PORTB_IRQn
+    #define RESET_BUTTON_GPIO        GPIOB
+    #define RESET_BUTTON_PORT        PORTB
+    #define RESET_BUTTON_CLOCK       kCLOCK_PortB
+    #define RESET_BUTTON_PIN         1
+    #define RESET_BUTTON_IRQ         PORTB_IRQn
     #define RESET_BUTTON_IRQ_HANDLER PORTB_IRQHandler
 
     #define RESET_BUTTON_IS_PRESSED !GPIO_ReadPinInput(RESET_BUTTON_GPIO, RESET_BUTTON_PIN)
