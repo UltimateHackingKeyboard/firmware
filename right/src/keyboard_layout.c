13783a89e353658ce0f63b67fe13f79a5bd70cd2
Gergely Nagy <algernon@madhouse-project.org>
2016-12-11 09:15:15 +0100

    Use 16-bits to describe each key
    Turns the keymap into an array of `uhk_key_t` elements, in order to be able to
    describe behaviour outside of simple keys. As a test, the `Fn` key on the right
    side now acts as a test key, and will toggle the LEDs on and off.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- right/src/keyboard_layout.c
+++ right/src/keyboard_layout.c
@@ -1,25 +1,26 @@
 #include "keyboard_layout.h"
+#include "led_driver.h"
 
 static uint8_t keyMasks[LAYOUT_KEY_COUNT];
 
-static inline __attribute__((always_inline)) uint8_t getKeycode(KEYBOARD_LAYOUT(layout), uint8_t keyId, uint8_t modifierState)
+static inline __attribute__((always_inline)) uhk_key_t getKeycode(KEYBOARD_LAYOUT(layout), uint8_t keyId, uint8_t modifierState)
 {
 	if (keyId<LAYOUT_KEY_COUNT) {
 		if (keyMasks[keyId]!=0 && keyMasks[keyId]!=modifierState){
 			//Mask out key presses after releasing modifier keys
-			return 0;
+      return (uhk_key_t) { .raw = 0 };
 		}
 
-		uint8_t k = layout[keyId][modifierState];
+		uhk_key_t k = layout[keyId][modifierState];
 		keyMasks[keyId] = modifierState;
 
-		if (k==0) {
+		if (k.raw==0) {
 			k = layout[keyId][0];
 		}
 
 		return k;
 	} else {
-		return 0;
+		return (uhk_key_t) { .raw = 0 };
 	}
 }
 
@@ -49,6 +50,24 @@ static void clearKeymasks(const uint8_t *leftKeyStates, const uint8_t *rightKeyS
 	}
 }
 
+static bool isEnabled = true;
+
+void handleKey(uhk_key_t key, int scancodeIdx, usb_keyboard_report_t *report) {
+  switch (key.type) {
+  case UHK_KEY_SIMPLE:
+    if (key.key) {
+      report->scancodes[scancodeIdx++] = key.key;
+    }
+    break;
+  case UHK_KEY_TEST:
+    LedDriver_InitAllLeds(isEnabled);
+    isEnabled = !isEnabled;
+    break;
+  default:
+    break;
+  }
+}
+
 void fillKeyboardReport(usb_keyboard_report_t *report, const uint8_t *leftKeyStates, const uint8_t *rightKeyStates, KEYBOARD_LAYOUT(layout)){
 	int scancodeIdx = 0;
 
@@ -61,10 +80,9 @@ void fillKeyboardReport(usb_keyboard_report_t *report, const uint8_t *leftKeySta
 		}
 
 		if (rightKeyStates[keyId]) {
-			uint8_t code=getKeycode(layout, keyId, modifierState);
-			if (code) {
-				report->scancodes[scancodeIdx++] = code;
-			}
+			uhk_key_t code=getKeycode(layout, keyId, modifierState);
+
+      handleKey(code, scancodeIdx, report);
 		}
 	}
 
@@ -74,10 +92,9 @@ void fillKeyboardReport(usb_keyboard_report_t *report, const uint8_t *leftKeySta
 		}
 
 		if (leftKeyStates[keyId]) {
-			uint8_t code=getKeycode(layout, LAYOUT_LEFT_OFFSET+keyId, modifierState);
-			if (code) {
-				report->scancodes[scancodeIdx++] = code;
-			}
+			uhk_key_t code=getKeycode(layout, LAYOUT_LEFT_OFFSET+keyId, modifierState);
+
+      handleKey(code, scancodeIdx, report);
 		}
 	}
 }
--- right/src/keyboard_layout.h
+++ right/src/keyboard_layout.h
@@ -27,7 +27,29 @@
 
 #define LAYOUT_LEFT_OFFSET KEY_STATE_COUNT
 
-#define KEYBOARD_LAYOUT(name) const uint8_t name[LAYOUT_KEY_COUNT][LAYOUT_MOD_COUNT]
+typedef enum {
+  UHK_KEY_SIMPLE,
+  UHK_KEY_SWITCH_LAYER,
+  UHK_KEY_MOUSE,
+  UHK_KEY_SWITCH_KEYMAP,
+  UHK_KEY_PLAY_MACRO,
+
+  UHK_KEY_TEST,
+} uhk_key_type_t;
+
+typedef union {
+  struct {
+    uhk_key_type_t type;
+    union {
+      uint8_t value;
+      uint8_t key;
+    };
+  };
+  uint16_t raw;
+} uhk_key_t;
+
+#define Key_NoKey {.raw = 0}
+#define KEYBOARD_LAYOUT(name) const uhk_key_t name[LAYOUT_KEY_COUNT][LAYOUT_MOD_COUNT]
 
 #define KEYID_LEFT_MOD 33
 #define KEYID_LEFT_FN 31
