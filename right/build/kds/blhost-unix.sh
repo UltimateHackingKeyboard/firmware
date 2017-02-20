b4532975704e5538650f3b82677ec4c0c5245656
László Monda <laci@monda.hu>
2017-02-20 18:30:30 +0100

    Add the v7-release-srec build configuration. Add the "uhk-right v7 debug kboot" run configuration. Make blhost-unix.sh expect the srec file to be transferred.

--- right/build/kds/blhost-unix.sh
+++ right/build/kds/blhost-unix.sh
@@ -4,5 +4,5 @@ set -x # echo on
 jump-to-bootloader.js
 sleep 2 &&
 blhost --usb 0x15a2,0x0073 flash-erase-all 0 &&
-blhost --usb 0x15a2,0x0073 flash-image v7-release-srec/uhk-right.srec &&
+blhost --usb 0x15a2,0x0073 flash-image $1 &&
 blhost --usb 0x15a2,0x0073 reset
