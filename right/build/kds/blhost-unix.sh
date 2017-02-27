797c51ca7a0f35419f1e414fbd2d1dde1c23c5a9
László Monda <laci@monda.hu>
2017-02-28 00:13:31 +0100

    Rework blhost-unix.sh a bit now that jump-to-bootloader.js times out and returns a relevant return code.

--- right/build/kds/blhost-unix.sh
+++ right/build/kds/blhost-unix.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -x # echo on
 
-jump-to-bootloader.js
-sleep 2 &&
+jump-to-bootloader.js &&
 blhost --usb 0x15a2,0x0073 flash-erase-all 0 &&
 blhost --usb 0x15a2,0x0073 flash-image $1 &&
 blhost --usb 0x15a2,0x0073 reset
