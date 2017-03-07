15388640c8f35f2d1a3fc5a559ceab1b91da589d
László Monda <laci@monda.hu>
2017-03-07 22:11:18 +0100

    Better organize and name HID macros.

--- right/src/usb_api.h
+++ right/src/usb_api.h
@@ -39,12 +39,10 @@
     // HID report item related constants
 
     #define HID_RI_USAGE_PAGE_GENERIC_DESKTOP 0x01
+    #define HID_RI_USAGE_PAGE_KEY_CODES       0x07
     #define HID_RI_USAGE_PAGE_LEDS            0x08
     #define HID_RI_USAGE_PAGE_BUTTONS         0x09
     #define HID_RI_USAGE_PAGE_CONSUMER        0x0C
-
-    #define HID_RI_USAGE_ID_CONSUMER_PAGE_CONSUMER_CONTROL      0x01
-    #define HID_RI_USAGE_ID_GENERIC_DESKTOP_PAGE_SYSTEM_CONTROL 0x80
 
     #define HID_RI_USAGE_GENERIC_DESKTOP_POINTER               0x01
     #define HID_RI_USAGE_GENERIC_DESKTOP_MOUSE                 0x02
@@ -56,13 +54,12 @@
     #define HID_RI_USAGE_GENERIC_DESKTOP_Y                     0x31
     #define HID_RI_USAGE_GENERIC_DESKTOP_WHEEL                 0x38
     #define HID_RI_USAGE_GENERIC_DESKTOP_RESOLUTION_MULTIPLIER 0x48
-
-    #define HID_RI_USAGE_CONSUMER_AC_PAN 0x0238
+    #define HID_RI_USAGE_GENERIC_DESKTOP_SYSTEM_CONTROL        0x80
+    #define HID_RI_USAGE_CONSUMER_CONTROL                      0x01
+    #define HID_RI_USAGE_CONSUMER_AC_PAN                       0x0238
 
     #define HID_RI_COLLECTION_PHYSICAL    0x00
     #define HID_RI_COLLECTION_APPLICATION 0x01
     #define HID_RI_COLLECTION_LOGICAL     0x02
-
-    #define HID_RI_USAGE_PAGE_KEY_CODES 0x07
 
 #endif
