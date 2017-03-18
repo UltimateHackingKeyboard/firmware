dd71e79dda90d53771ba4c2c594a62b0b0a3460c
László Monda <laci@monda.hu>
2017-03-18 22:41:48 +0100

    Remove the commented out hardcoded LED PWM brightness.

--- right/src/bridge_slaves/bridge_slave_uhk_module.c
+++ right/src/bridge_slaves/bridge_slave_uhk_module.c
@@ -13,7 +13,6 @@ uint8_t txBuffer[2];
 bool BridgeSlaveUhkModuleHandler(uint8_t uhkModuleId) {
     uhk_module_state_t *uhkModuleInternalState = UhkModuleStates + uhkModuleId;
     uhk_module_state_t *uhkModuleExternalState = uhkModuleExternalStates + uhkModuleId;
-    //uhkModuleInternalState->ledPwmBrightness = 0xff;
 
     switch (currentUhkModuleField) {
         case UhkModuleField_SendKeystatesRequestCommand:
