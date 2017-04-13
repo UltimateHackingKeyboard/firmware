8d29432ce0dbaee95d115d446bf2b260d4b18879
László Monda <laci@monda.hu>
2017-04-13 23:32:07 +0200

    Move the content of bootloader.h into serial_packet.c. Delete bootloader.h

--- right/src/buspal/packet/serial_packet.c
+++ right/src/buspal/packet/serial_packet.c
@@ -1,5 +1,20 @@
-#include "bootloader/bootloader.h"
+#include "command.h"
 #include "crc16.h"
+
+enum _bootloader_status {
+    kStatus_UnknownCommand = MAKE_STATUS(kStatusGroup_Bootloader, 0),
+    kStatus_SecurityViolation = MAKE_STATUS(kStatusGroup_Bootloader, 1),
+    kStatus_AbortDataPhase = MAKE_STATUS(kStatusGroup_Bootloader, 2),
+    kStatus_Ping = MAKE_STATUS(kStatusGroup_Bootloader, 3),
+    kStatus_NoResponse = MAKE_STATUS(kStatusGroup_Bootloader, 4),
+    kStatus_NoResponseExpected = MAKE_STATUS(kStatusGroup_Bootloader, 5),
+};
+
+typedef struct _bootloaderContext {
+    const peripheral_descriptor_t *activePeripheral;
+} bootloader_context_t;
+
+extern bootloader_context_t g_bootloaderContext;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Prototypes
