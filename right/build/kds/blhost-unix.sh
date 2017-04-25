f263f0a9f918584d26096bd2b26ee49129769212
László Monda <laci@monda.hu>
2017-04-25 18:15:46 +0200

    Extract the usb parameters of blhost.

--- right/build/kds/blhost-unix.sh
+++ right/build/kds/blhost-unix.sh
@@ -4,7 +4,7 @@ set -e # fail the script if a command fails
 firmware_image=`pwd`/$1
 usb_dir=../../../lib/agent/usb
 usb_binding=$usb_dir/node_modules/usb/build/Release/usb_bindings.node
-blhost=../../../lib/bootloader/bin/Tools/blhost/linux/amd64/blhost
+blhost="../../../lib/bootloader/bin/Tools/blhost/linux/amd64/blhost --usb 0x15a2,0x0073"
 
 set -x # echo on
 
@@ -14,7 +14,7 @@ if [ ! -f $usb_binding ]; then
 fi
 
 $usb_dir/jump-to-bootloader.js
-$blhost --usb 0x15a2,0x0073 flash-security-disable 0403020108070605
-$blhost --usb 0x15a2,0x0073 flash-erase-all 0
-$blhost --usb 0x15a2,0x0073 flash-image $firmware_image
-$blhost --usb 0x15a2,0x0073 reset
+$blhost flash-security-disable 0403020108070605
+$blhost flash-erase-all 0
+$blhost flash-image $firmware_image
+$blhost reset
