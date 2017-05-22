2b08775e4e2cc29a2da30acdd91c479f885e9d86
László Monda <laci@monda.hu>
2017-05-23 01:51:46 +0200

    Indent array elements nicely.

--- left/src/main.c
+++ left/src/main.c
@@ -7,19 +7,19 @@ key_matrix_t keyMatrix = {
     .rowNum = KEYBOARD_MATRIX_ROWS_NUM,
     .cols = (key_matrix_pin_t[]) {
         {PORTB, GPIOB, kCLOCK_PortB, 11},
-        {PORTA, GPIOA, kCLOCK_PortA, 6},
-        {PORTA, GPIOA, kCLOCK_PortA, 8},
-        {PORTB, GPIOB, kCLOCK_PortB, 0},
-        {PORTB, GPIOB, kCLOCK_PortB, 6},
-        {PORTA, GPIOA, kCLOCK_PortA, 3},
+        {PORTA, GPIOA, kCLOCK_PortA,  6},
+        {PORTA, GPIOA, kCLOCK_PortA,  8},
+        {PORTB, GPIOB, kCLOCK_PortB,  0},
+        {PORTB, GPIOB, kCLOCK_PortB,  6},
+        {PORTA, GPIOA, kCLOCK_PortA,  3},
         {PORTA, GPIOA, kCLOCK_PortA, 12}
     },
     .rows = (key_matrix_pin_t[]) {
-        {PORTB, GPIOB, kCLOCK_PortB, 7},
+        {PORTB, GPIOB, kCLOCK_PortB,  7},
         {PORTB, GPIOB, kCLOCK_PortB, 10},
-        {PORTA, GPIOA, kCLOCK_PortA, 5},
-        {PORTA, GPIOA, kCLOCK_PortA, 7},
-        {PORTA, GPIOA, kCLOCK_PortA, 4}
+        {PORTA, GPIOA, kCLOCK_PortA,  5},
+        {PORTA, GPIOA, kCLOCK_PortA,  7},
+        {PORTA, GPIOA, kCLOCK_PortA,  4}
     }
 };
 
