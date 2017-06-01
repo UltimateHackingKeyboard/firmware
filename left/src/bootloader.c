6e23df41bf8e8adca9f64886ed50aa255c75880b
László Monda <laci@monda.hu>
2017-06-01 03:04:43 +0200

    Set .peripheralDetectionTimeoutMs to 300 ms.

--- left/src/bootloader.c
+++ left/src/bootloader.c
@@ -4,7 +4,7 @@ __attribute__((used, section(".BootloaderConfig"))) const bootloader_config_t Bo
     .tag = 0x6766636B,                    // Magic Number
     .enabledPeripherals = 0xE2,           // Enabled Peripheral: I2C
     .i2cSlaveAddress = 0x10,              // Use user-defined I2C address
-    .peripheralDetectionTimeoutMs = 3000, // Use user-defined timeout (ms)
+    .peripheralDetectionTimeoutMs = 300,  // Use user-defined timeout (ms)
     .clockFlags = 0xFF,                   // Disable High speed mode
     .clockDivider = 0xFF,                 // Use clock divider (0)
 };
