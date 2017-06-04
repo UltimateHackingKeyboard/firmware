a6ed51360f8ecf2f7e512b33682282cd6fa67894
László Monda <laci@monda.hu>
2017-06-04 23:03:50 +0200

    Let's not mess around with SP.

--- right/src/wormhole.c
+++ right/src/wormhole.c
@@ -1,16 +1,3 @@
 #include "wormhole.h"
 
 wormhole_t *Wormhole NO_INIT_GCC;
-
-void* getSP(void)
-{
-    void *sp;
-    __asm__ __volatile__ ("mov %0, sp" : "=r"(sp));
-    return sp;
-}
-
-void __attribute__ ((__section__ (".init3"), __naked__)) move_sp(void) {
-    void* SP = getSP();
-    SP -= sizeof(wormhole_t);
-    Wormhole = (wormhole_t*)(SP + 1);
-}
