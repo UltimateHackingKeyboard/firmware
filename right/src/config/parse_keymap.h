431b2f2f185e4b8c9b1180d74444a9fe8848f8e4
László Monda <laci@monda.hu>
2017-06-15 18:17:59 +0200

    Fix file structure according to our coding standards.

--- right/src/config/parse_keymap.h
+++ right/src/config/parse_keymap.h
@@ -1,8 +1,12 @@
 #ifndef __PARSE_KEYMAP_H__
 #define __PARSE_KEYMAP_H__
 
-#include <stdint.h>
+// Includes:
 
-void ParseLayer(const uint8_t *data, uint8_t targetLayer);
+    #include <stdint.h>
+
+// Functions:
+
+    void ParseLayer(const uint8_t *data, uint8_t targetLayer);
 
 #endif
