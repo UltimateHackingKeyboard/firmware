e6454079b9f98faa565a01e4e600b9052e03f3e3
László Monda <laci@monda.hu>
2017-06-27 09:05:26 +0200

    Fix reset button pin number.

--- right/src/peripherals/reset_button.h
+++ right/src/peripherals/reset_button.h
@@ -10,7 +10,7 @@
     #define RESET_BUTTON_GPIO        GPIOB
     #define RESET_BUTTON_PORT        PORTB
     #define RESET_BUTTON_CLOCK       kCLOCK_PortB
-    #define RESET_BUTTON_PIN         1
+    #define RESET_BUTTON_PIN         2
     #define RESET_BUTTON_IRQ         PORTB_IRQn
     #define RESET_BUTTON_IRQ_HANDLER PORTB_IRQHandler
 
