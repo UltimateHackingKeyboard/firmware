25181e584192f3be808606a6dd2875101cac359e
Eric Tang <e_l_tang@outlook.com>
2017-07-13 16:37:58 -0700

    Expose the new API for controlling the LED display

--- right/src/led_display.c
+++ right/src/led_display.c
@@ -1,13 +1,10 @@
 #include "led_display.h"
-#include "layer.h"
 
-#define LAYER_LED_FIRST    FRAME_REGISTER_PWM_FIRST + 13
-#define LAYER_LED_DISTANCE 16
+void LedDisplay_SetText(uint8_t length, const char* text) {
+}
 
-uint8_t LedDisplayBrightness = 0xff;
+void LedDisplay_SetLayer(uint8_t layerId) {
+}
 
-void LedDisplay_SetLayerLed(uint8_t layerId) {
-    for (uint8_t i = 0; i < LAYER_COUNT; i++) {
-//        LedDriver_WriteRegister(I2C_ADDRESS_LED_DRIVER_LEFT, LAYER_LED_FIRST + (i * LAYER_LED_DISTANCE), LedDisplayBrightness * (layerId == i + 1));
-    }
+void LedDisplay_SetIcon(led_display_icon_t icon, bool isEnabled) {
 }
