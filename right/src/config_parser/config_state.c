d035c8699b02ae28f94dcb3aea0dbb8b2e4ee405
Eric Tang <e_l_tang@outlook.com>
2017-08-07 16:40:21 -0700

    Try putting userConfig2 in the upper half of the RAM

--- right/src/config_parser/config_state.c
+++ right/src/config_parser/config_state.c
@@ -4,7 +4,7 @@ static uint8_t hardwareConfig[HARDWARE_CONFIG_SIZE];
 config_buffer_t HardwareConfigBuffer = {hardwareConfig};
 
 static uint8_t userConfig1[USER_CONFIG_SIZE];
-static uint8_t userConfig2[USER_CONFIG_SIZE];
+static uint8_t __attribute__((section (".m_data_2"))) userConfig2[USER_CONFIG_SIZE];
 
 config_buffer_t UserConfigBuffer = { userConfig1 };
 config_buffer_t NewUserConfigBuffer = { userConfig2 };
