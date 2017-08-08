036b5c41725b440742c0a9d9e17bdb2975a6cce9
Eric Tang <e_l_tang@outlook.com>
2017-08-07 17:39:14 -0700

    Cut the lengths of userConfig1 and userConfig2 in half for now

--- right/src/config_parser/config_state.c
+++ right/src/config_parser/config_state.c
@@ -3,8 +3,8 @@
 static uint8_t hardwareConfig[HARDWARE_CONFIG_SIZE];
 config_buffer_t HardwareConfigBuffer = {hardwareConfig};
 
-static uint8_t userConfig1[USER_CONFIG_SIZE];
-static uint8_t __attribute__((section (".m_data_2"))) userConfig2[USER_CONFIG_SIZE];
+static uint8_t userConfig1[USER_CONFIG_SIZE / 2];
+static uint8_t __attribute__((section (".m_data_2"))) userConfig2[USER_CONFIG_SIZE / 2];
 
 config_buffer_t UserConfigBuffer = { userConfig1 };
 config_buffer_t NewUserConfigBuffer = { userConfig2 };
