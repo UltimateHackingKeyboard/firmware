35102918b2b5d04313f805feed7c79467fcd7c22
László Monda <laci@monda.hu>
2017-08-19 12:09:08 +0200

    Only set IsI2cTransferScheduled to true if the previous I2C operation returned with kStatus_Success

--- right/src/i2c.c
+++ right/src/i2c.c
@@ -10,8 +10,7 @@ void I2cAsyncWrite(uint8_t i2cAddress, uint8_t *data, size_t dataSize)
     masterTransfer.direction = kI2C_Write;
     masterTransfer.data = data;
     masterTransfer.dataSize = dataSize;
-    I2C_MasterTransferNonBlocking(I2C_MAIN_BUS_BASEADDR, &I2cMasterHandle, &masterTransfer);
-    IsI2cTransferScheduled = true;
+    IsI2cTransferScheduled = I2C_MasterTransferNonBlocking(I2C_MAIN_BUS_BASEADDR, &I2cMasterHandle, &masterTransfer) == kStatus_Success;
 }
 
 void I2cAsyncRead(uint8_t i2cAddress, uint8_t *data, size_t dataSize)
@@ -20,6 +19,5 @@ void I2cAsyncRead(uint8_t i2cAddress, uint8_t *data, size_t dataSize)
     masterTransfer.direction = kI2C_Read;
     masterTransfer.data = data;
     masterTransfer.dataSize = dataSize;
-    I2C_MasterTransferNonBlocking(I2C_MAIN_BUS_BASEADDR, &I2cMasterHandle, &masterTransfer);
-    IsI2cTransferScheduled = true;
+    IsI2cTransferScheduled = I2C_MasterTransferNonBlocking(I2C_MAIN_BUS_BASEADDR, &I2cMasterHandle, &masterTransfer) == kStatus_Success;
 }
