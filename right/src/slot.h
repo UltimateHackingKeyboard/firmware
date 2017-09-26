1839be81b63bb9fa084ae874f8e9f87d0b366881
László Monda <laci@monda.hu>
2017-09-26 20:08:57 +0200

    Improve the description of slots.

--- right/src/slot.h
+++ right/src/slot.h
@@ -1,14 +1,14 @@
 #ifndef __SLOT_H__
 #define __SLOT_H__
 
-// A slot is a physical space that can be occupied by a module. No more than a single module
-// can occupy a slot. Think of the pogo pin connector of the left keyboard half or the right
-// keyboard half. Given their physical design, it's impossible to mount two modules to a slot.
+// A slot is a dedicated physical space that can only be occupied by a single module.
+// Think of the pogo pin connector of the left keyboard half, for example.
+// Given its physical design, it's impossible to mount two modules to a slot.
 //
 // Slots are useful for two reasons:
-// 1. Every slot has a dedicated I2C address to avoid the I2C address collision of modules.
-// 2. Slots denote the maximum number of modules that can be mounted at a given time, allowing
-//    for the allocation of static memory structures for modules.
+// 1. Every slot has a dedicated I2C address. This avoids the I2C address collision of modules.
+// 2. There are a limited number of slots available which translates to a maximum number of modules
+//    that can be mounted, allowing for the allocation of static memory structures for modules.
 
 // Macros:
 
