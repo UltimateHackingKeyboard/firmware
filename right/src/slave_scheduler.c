8d57968575ae6d09c3e254e0f4fc9d648c116694
László Monda <laci@monda.hu>
2017-09-28 03:35:28 +0200

    Also commit slave_scheduler.c regarding the kStatus_Uhk_NoOp -> kStatus_Uhk_NoTransfer rename that I forgot to commit.

--- right/src/slave_scheduler.c
+++ right/src/slave_scheduler.c
@@ -34,14 +34,14 @@ static void masterCallback(I2C_Type *base, i2c_master_handle_t *handle, status_t
         }
 
         status_t currentStatus = currentSlave->update(currentSlave->perDriverId);
-        isTransferScheduled = currentStatus != kStatus_Uhk_IdleSlave && currentStatus != kStatus_Uhk_NoOp;
+        isTransferScheduled = currentStatus != kStatus_Uhk_IdleSlave && currentStatus != kStatus_Uhk_NoTransfer;
         //isTransferScheduled = currentStatus == kStatus_Success // Why it is not working?
         if (isTransferScheduled) {
             currentSlave->isConnected = true;
         }
 
         previousSlaveId = currentSlaveId;
-        if (currentStatus != kStatus_Uhk_NoOp) {
+        if (currentStatus != kStatus_Uhk_NoTransfer) {
             currentSlaveId++;
         }
 
