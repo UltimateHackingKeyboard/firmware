8b5b224bf3e4d2574bcaf17d00563a3e50063c03
László Monda <laci@monda.hu>
2017-09-28 04:05:44 +0200

    Compute KEY_STATE_SIZE the right way.

--- right/src/slave_drivers/uhk_module_driver.h
+++ right/src/slave_drivers/uhk_module_driver.h
@@ -9,7 +9,7 @@
 // Macros:
 
     #define UHK_MODULE_MAX_COUNT 3
-    #define KEY_STATE_SIZE (LEFT_KEYBOARD_HALF_KEY_COUNT/8 + 1)
+    #define KEY_STATE_SIZE (LEFT_KEYBOARD_HALF_KEY_COUNT/8 + (LEFT_KEYBOARD_HALF_KEY_COUNT % 8 ? 1 : 0))
     #define MAX_PWM_BRIGHTNESS 0x64
 
 // Typedefs:
