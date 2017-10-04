259f4d3299c4e583c992047e9013bec21195ff07
László Monda <laci@monda.hu>
2017-10-04 23:57:43 +0200

    Add config_buffer_id_t.

--- right/src/config_parser/config_globals.h
+++ right/src/config_parser/config_globals.h
@@ -12,11 +12,19 @@
     #define HARDWARE_CONFIG_SIZE 64
     #define USER_CONFIG_SIZE (EEPROM_SIZE - HARDWARE_CONFIG_SIZE)
 
+// Typedefs:
+
+    typedef enum {
+        ConfigBufferId_HardwareConfig,
+        ConfigBufferId_StagingUserConfig,
+        ConfigBufferId_ValidatedUserConfig,
+    } config_buffer_id_t;
+
 // Variables:
 
     extern bool ParserRunDry;
     extern config_buffer_t HardwareConfigBuffer;
-    extern config_buffer_t ValidatedUserConfigBuffer;
     extern config_buffer_t StagingUserConfigBuffer;
+    extern config_buffer_t ValidatedUserConfigBuffer;
 
 #endif
