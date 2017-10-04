af52c017d85e5ca70f57cf277bead70bcfa984e4
László Monda <laci@monda.hu>
2017-10-04 13:02:40 +0200

    No need to assign a value to I2C_WATCHDOG. Just define it.

--- right/src/config.h
+++ right/src/config.h
@@ -3,7 +3,7 @@
 
 // Macros:
 
-    #define I2C_WATCHDOG 1
+    #define I2C_WATCHDOG
 //    #define LED_DRIVER_STRESS_TEST
     #define FIXED_BUSPAL_BOOTLOADER  1 // Used to mark the fixed BusPal bootloader. Macro usage can be removed in the future.
 //    #define FORCE_BUSPAL 1
