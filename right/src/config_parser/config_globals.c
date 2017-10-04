8e420f85922639844e0a86484081be8ad3bb2a1d
László Monda <laci@monda.hu>
2017-10-04 22:39:40 +0200

    Rename and rearrange some variables in config_globals.c for better readability.

--- right/src/config_parser/config_globals.c
+++ right/src/config_parser/config_globals.c
@@ -2,11 +2,11 @@
 #include "attributes.h"
 
 static uint8_t hardwareConfig[HARDWARE_CONFIG_SIZE];
-config_buffer_t HardwareConfigBuffer = {hardwareConfig};
+static uint8_t ATTR_DATA2 stagingUserConfig[USER_CONFIG_SIZE];
+static uint8_t validatedUserConfig[USER_CONFIG_SIZE];
 
-static uint8_t userConfig1[USER_CONFIG_SIZE];
-static uint8_t ATTR_DATA2 userConfig2[USER_CONFIG_SIZE];
-config_buffer_t ValidatedUserConfigBuffer = { userConfig1 };
-config_buffer_t StagingUserConfigBuffer = { userConfig2 };
+config_buffer_t HardwareConfigBuffer = { hardwareConfig };
+config_buffer_t StagingUserConfigBuffer = { stagingUserConfig };
+config_buffer_t ValidatedUserConfigBuffer = { validatedUserConfig };
 
 bool ParserRunDry;
