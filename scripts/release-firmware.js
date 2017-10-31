561d183840aa8ea4ad98bc476e78032ab67e6028
László Monda <laci@monda.hu>
2017-11-01 00:11:33 +0100

    Make release-firmware.js work when invoked from any directory.

--- scripts/release-firmware.js
+++ scripts/release-firmware.js
@@ -4,18 +4,18 @@ require('shelljs/global');
 
 config.fatal = true;
 
-const version = JSON.parse(fs.readFileSync('package.json')).version;
+const version = JSON.parse(fs.readFileSync(`${__dirname}/package.json`)).version;
 const releaseName = `uhk-firmware-${version}`;
 const releaseDir = `/tmp/${releaseName}`;
-const rightFirmwareFile = '../right/build/uhk60-right_release/uhk-right.hex';
-const leftFirmwareFile = '../left/build/uhk60-left_release/uhk-left.bin';
+const rightFirmwareFile = `${__dirname}/../right/build/uhk60-right_release/uhk-right.hex`;
+const leftFirmwareFile = `${__dirname}/../left/build/uhk60-left_release/uhk-left.bin`;
 
 exec(`/opt/Freescale/KDS_v3/eclipse/kinetis-design-studio \
 --launcher.suppressErrors \
 -noSplash \
 -application org.eclipse.cdt.managedbuilder.core.headlessbuild \
--import ../left/build \
--import ../right/build \
+-import ${__dirname}/../left/build \
+-import ${__dirname}/../right/build \
 -cleanBuild uhk-left \
 -cleanBuild uhk-right`);
 
@@ -25,5 +25,5 @@ rm('-r', releaseDir);
 mkdir(releaseDir);
 cp(rightFirmwareFile, releaseDir);
 cp(leftFirmwareFile, releaseDir);
-cp('package.json', releaseDir);
-exec(`tar -cvjSf ${releaseName}.tar.bz2 -C ${releaseDir} .`);
+cp(`${__dirname}/package.json`, releaseDir);
+exec(`tar -cvjSf ${__dirname}/${releaseName}.tar.bz2 -C ${releaseDir} .`);
