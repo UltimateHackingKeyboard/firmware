76d319ec19e0fd15367895bfee63b6286fc64bc3
László Monda <laci@monda.hu>
2018-01-11 23:44:59 +0100

    Remove comment that says that 100 kHz works with a 20 meter long bridge cable. It was true for UART, not so much for I2C.

--- right/src/i2c.h
+++ right/src/i2c.h
@@ -13,7 +13,7 @@
     #define I2C_MAIN_BUS_BASEADDR  I2C0
     #define I2C_MAIN_BUS_IRQ_ID    I2C0_IRQn
     #define I2C_MAIN_BUS_CLK_SRC   I2C0_CLK_SRC
-    #define I2C_MAIN_BUS_BAUD_RATE 100000  // 100 kHz works even with a 20 meter long bridge cable.
+    #define I2C_MAIN_BUS_BAUD_RATE 100000
     #define I2C_MAIN_BUS_MUX       kPORT_MuxAlt7
 
     #define I2C_MAIN_BUS_SDA_GPIO  GPIOD
