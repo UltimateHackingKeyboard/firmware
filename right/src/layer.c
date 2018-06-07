2c91ef51db0c22370b1a66a821076d5993139154
László Monda <laci@monda.hu>
2018-06-07 04:10:48 +0200

    Make releasing locked layers possible again.

--- right/src/layer.c
+++ right/src/layer.c
@@ -20,7 +20,8 @@ void updateLayerStates(void)
                 if (action.type == KeyActionType_SwitchLayer) {
                     if (action.switchLayer.mode != SwitchLayerMode_Toggle) {
                         heldLayers[action.switchLayer.layer] = true;
-                    } else if (!keyState->previous && keyState->current) {
+                    }
+                    if (action.switchLayer.mode != SwitchLayerMode_Hold && !keyState->previous && keyState->current) {
                         toggledLayers[action.switchLayer.layer] = true;
                     }
                 }
