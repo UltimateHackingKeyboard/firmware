5572952dc8f62093f9c09b93cf8f726800b38830
László Monda <laci@monda.hu>
2018-08-05 22:28:30 +0200

    Use one liner comment style.

--- left/src/i2c_watchdog.c
+++ left/src/i2c_watchdog.c
@@ -5,31 +5,30 @@
 #include "init_peripherals.h"
 #include "config.h"
 
-/* NOTE: Because of a bug in the ROM bootloader of the KL03Z, the watchdog timer is disabled and cannot be re-enabled.
- * See https://community.nxp.com/thread/457893
- * Therefore the hardware watchdog timer cannot be used without an extra way to enter bootloader or application mode.
- */
-  static uint32_t prevWatchdogCounter = 0;
-  static uint32_t I2cWatchdog_RecoveryCounter; /* counter for how many times we had to recover and restart */
+// NOTE: Because of a bug in the ROM bootloader of the KL03Z, the watchdog timer is disabled and cannot be re-enabled.
+// See https://community.nxp.com/thread/457893
+// Therefore the hardware watchdog timer cannot be used without an extra way to enter bootloader or application mode.
+static uint32_t prevWatchdogCounter = 0;
+static uint32_t I2cWatchdog_RecoveryCounter; // Counter for how many times we had to recover and restart
 
 void RunWatchdog(void)
 {
-    static volatile uint32_t I2cWatchdog_WatchCounter = 0; /* counter for timer */
+    static volatile uint32_t I2cWatchdog_WatchCounter = 0; // Counter for timer
     static int cntr = 0;
 
     cntr++;
-    if (cntr==100) { /* we get called from KEY_SCANNER_HANDLER() which runs at 1ms, thus scaling down by 100 here to get 100 ms period */
+    if (cntr==100) { // We get called from KEY_SCANNER_HANDLER() which runs at 1ms, thus scaling down by 100 here to get 100 ms period
         cntr=0;
         TestLed_Toggle();
         I2cWatchdog_WatchCounter++;
 
-        if (I2cWatchdog_WatchCounter>10) { /* do not check within the first 1000 ms, as I2C might not be running yet */
+        if (I2cWatchdog_WatchCounter>10) { // Do not check within the first 1000 ms, as I2C might not be running yet
             if (I2C_Watchdog == prevWatchdogCounter) { // Restart I2C if there hasn't been any interrupt during 100 ms. I2C_Watchdog gets incremented for every I2C transaction
                 I2cWatchdog_RecoveryCounter++;
                 I2C_SlaveDeinit(I2C_BUS_BASEADDR);
                 initI2c();
             }
         }
-        prevWatchdogCounter = I2C_Watchdog; /* remember previous counter */
+        prevWatchdogCounter = I2C_Watchdog;
     }
 }
