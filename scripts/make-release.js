2b8156cad894e733f0549cd7eda27607eefcad23
Róbert Kiss <ert78gb@gmail.com>
2019-08-23 19:22:10 +0200

    chore: refactor the make-release.js file

--- scripts/make-release.js
+++ scripts/make-release.js
@@ -11,10 +11,8 @@ const package = JSON.parse(fs.readFileSync(`${__dirname}/package.json`));
 const version = package.firmwareVersion;
 const releaseName = `uhk-firmware-${version}`;
 const releaseDir = `${__dirname}/${releaseName}`;
-const modulesDir = `${releaseDir}/modules`;
 const releaseFile = `${__dirname}/${releaseName}.tar.bz2`;
-const leftFirmwareFile = `${__dirname}/../left/build_make/uhk_left.bin`;
-const usbDir = `${__dirname}/../lib/agent/packages/usb`;
+const agentDir = `${__dirname}/../lib/agent`;
 
 const deviceSourceFirmwares = package.devices.map(device => `${__dirname}/../${device.source}`);
 const moduleSourceFirmwares = package.modules.map(module => `${__dirname}/../${module.source}`);
@@ -23,14 +21,16 @@ rm('-rf', releaseDir, releaseFile, deviceSourceFirmwares, moduleSourceFirmwares)
 exec(`cd ${__dirname}/../left; make clean; make -j8`);
 exec(`cd ${__dirname}/../right; make clean; make -j8`);
 
+exec(`git pull origin master; git checkout master`, { cwd: agentDir });
+exec(`npm ci`, { cwd: agentDir });
+
 for (const device of package.devices) {
     const deviceDir = `${releaseDir}/devices/${device.name}`;
     const deviceSource = `${__dirname}/../${device.source}`;
     mkdir('-p', deviceDir);
     chmod(644, deviceSource);
     cp(deviceSource, `${deviceDir}/firmware.hex`);
-    exec(`cd ${usbDir}; git pull origin master; git checkout master`);
-    exec(`${usbDir}/user-config-json-to-bin.ts ${deviceDir}/config.bin`);
+    exec(`npm run convert-user-config-to-bin -- ${deviceDir}/config.bin`, { cwd: agentDir });
 }
 
 for (const module of package.modules) {
