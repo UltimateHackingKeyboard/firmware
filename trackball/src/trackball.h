4d6c8cadf5e23c4c5b705e4900add3bee44e854d
László Monda <laci@monda.hu>
2019-12-10 00:14:05 +0100

    Initialize SPI pins and mux.

--- trackball/src/trackball.h
+++ trackball/src/trackball.h
@@ -20,6 +20,24 @@
     #define TRACKBALL_NCS_CLOCK kCLOCK_PortB
     #define TRACKBALL_NCS_PIN 1
 
+    #define TRACKBALL_MOSI_PORT PORTA
+    #define TRACKBALL_MOSI_GPIO GPIOA
+    #define TRACKBALL_MOSI_IRQ PORTA_IRQn
+    #define TRACKBALL_MOSI_CLOCK kCLOCK_PortA
+    #define TRACKBALL_MOSI_PIN 7
+
+    #define TRACKBALL_MISO_PORT PORTA
+    #define TRACKBALL_MISO_GPIO GPIOA
+    #define TRACKBALL_MISO_IRQ PORTA_IRQn
+    #define TRACKBALL_MISO_CLOCK kCLOCK_PortA
+    #define TRACKBALL_MISO_PIN 6
+
+    #define TRACKBALL_SCK_PORT PORTB
+    #define TRACKBALL_SCK_GPIO GPIOB
+    #define TRACKBALL_SCK_IRQ PORTB_IRQn
+    #define TRACKBALL_SCK_CLOCK kCLOCK_PortB
+    #define TRACKBALL_SCK_PIN 0
+
 // Variables:
 
     extern pointer_delta_t Trackball_PointerDelta;
