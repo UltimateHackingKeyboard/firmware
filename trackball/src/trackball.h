ea30edfafe78f1fd8a4410d0b729931f760ca5f9
László Monda <laci@monda.hu>
2019-12-09 17:29:38 +0100

    Remove unused macros from trackball.h

--- trackball/src/trackball.h
+++ trackball/src/trackball.h
@@ -14,24 +14,6 @@
     #define TRACKBALL_SHTDWN_CLOCK kCLOCK_PortA
     #define TRACKBALL_SHTDWN_PIN 4
 
-    #define TRACKBALL_RIGHT_PORT PORTB
-    #define TRACKBALL_RIGHT_GPIO GPIOB
-    #define TRACKBALL_RIGHT_IRQ PORTB_IRQn
-    #define TRACKBALL_RIGHT_CLOCK kCLOCK_PortB
-    #define TRACKBALL_RIGHT_PIN 6
-
-    #define TRACKBALL_UP_PORT PORTA
-    #define TRACKBALL_UP_GPIO GPIOA
-    #define TRACKBALL_UP_IRQ PORTA_IRQn
-    #define TRACKBALL_UP_CLOCK kCLOCK_PortA
-    #define TRACKBALL_UP_PIN 3
-
-    #define TRACKBALL_DOWN_PORT PORTA
-    #define TRACKBALL_DOWN_GPIO GPIOA
-    #define TRACKBALL_DOWN_IRQ PORTA_IRQn
-    #define TRACKBALL_DOWN_CLOCK kCLOCK_PortA
-    #define TRACKBALL_DOWN_PIN 4
-
 // Variables:
 
     extern pointer_delta_t Trackball_PointerDelta;
