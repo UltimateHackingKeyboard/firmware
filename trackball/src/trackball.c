299b73be4a8580da87ad88169156bf7f9b877fb0
László Monda <laci@monda.hu>
2019-12-21 19:49:35 +0100

    Remove redundant GPIO_PinInit()

--- trackball/src/trackball.c
+++ trackball/src/trackball.c
@@ -69,7 +69,6 @@ void Trackball_Init(void)
 {
     CLOCK_EnableClock(TRACKBALL_SHTDWN_CLOCK);
     PORT_SetPinConfig(TRACKBALL_SHTDWN_PORT, TRACKBALL_SHTDWN_PIN, &(port_pin_config_t){.pullSelect=kPORT_PullDown, .mux=kPORT_MuxAsGpio});
-    GPIO_PinInit(TRACKBALL_SHTDWN_GPIO, TRACKBALL_SHTDWN_PIN, &(gpio_pin_config_t){kGPIO_DigitalOutput, 0});
     GPIO_WritePinOutput(TRACKBALL_SHTDWN_GPIO, TRACKBALL_SHTDWN_PIN, 0);
 
     CLOCK_EnableClock(TRACKBALL_NCS_CLOCK);
