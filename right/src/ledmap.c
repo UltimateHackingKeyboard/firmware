552ce12f9b6c82483dcb5d85bd729a7626338c86
László Monda <laci@monda.hu>
2020-05-28 23:52:44 +0200

    Update right keyboard half LED map ids.

--- right/src/ledmap.c
+++ right/src/ledmap.c
@@ -4,49 +4,49 @@ rgb_key_ids_t LedMap[SLOT_COUNT][MAX_KEY_COUNT_PER_MODULE] = {
     // Right keyboard half
     {
         // Row 1
-        { .red=0, .green=0, .blue=0 }, // 7
-        { .red=0, .green=0, .blue=0 }, // 8
-        { .red=0, .green=0, .blue=0 }, // 9
-        { .red=0, .green=0, .blue=0 }, // 0
-        { .red=0, .green=0, .blue=0 }, // -
-        { .red=0, .green=0, .blue=0 }, // =
-        { .red=0, .green=0, .blue=0 }, // Backspace
+        { .red=144, .green=160, .blue=32 }, // 7
+        { .red=145, .green=161, .blue=33 }, // 8
+        { .red=146, .green=162, .blue=34 }, // 9
+        { .red=147, .green=163, .blue=35 }, // 0
+        { .red=148, .green=164, .blue=36 }, // -
+        { .red=149, .green=165, .blue=37 }, // =
+        { .red=152, .green=168, .blue=0 }, // Backspace
 
         // Row 2
-        { .red=0, .green=0, .blue=0 }, // U
-        { .red=0, .green=0, .blue=0 }, // I
-        { .red=0, .green=0, .blue=0 }, // O
-        { .red=0, .green=0, .blue=0 }, // P
-        { .red=0, .green=0, .blue=0 }, // [
-        { .red=0, .green=0, .blue=0 }, // ]
-        { .red=0, .green=0, .blue=0 }, // Backslash
-        { .red=0, .green=0, .blue=0 }, // Y
+        { .red=153, .green=169, .blue=41 }, // U
+        { .red=154, .green=170, .blue=42 }, // I
+        { .red=155, .green=171, .blue=43 }, // O
+        { .red=156, .green=172, .blue=44 }, // P
+        { .red=96, .green=112, .blue=128 }, // [
+        { .red=97, .green=113, .blue=129 }, // ]
+        { .red=98, .green=114, .blue=130 }, // Backslash
+        { .red=157, .green=173, .blue=45 }, // Y
 
         // Row 3
-        { .red=0, .green=0, .blue=0 }, // J
-        { .red=0, .green=0, .blue=0 }, // K
-        { .red=0, .green=0, .blue=0 }, // L
-        { .red=0, .green=0, .blue=0 }, // ;
-        { .red=0, .green=0, .blue=0 }, // '
-        { .red=0, .green=0, .blue=0 }, // Enter
-        { .red=0, .green=0, .blue=0 }, // H
+        { .red=48, .green=64, .blue=80 }, // J
+        { .red=49, .green=65, .blue=81 }, // K
+        { .red=50, .green=66, .blue=82 }, // L
+        { .red=51, .green=67, .blue=83 }, // ;
+        { .red=52, .green=68, .blue=84 }, // '
+        { .red=53, .green=69, .blue=85 }, // Enter
+        { .red=61, .green=77, .blue=93 }, // H
 
         // Row 4
-        { .red=0, .green=0, .blue=0 }, // N
-        { .red=0, .green=0, .blue=0 }, // M
-        { .red=0, .green=0, .blue=0 }, // ,
-        { .red=0, .green=0, .blue=0 }, // .
-        { .red=0, .green=0, .blue=0 }, // /
-        { .red=0, .green=0, .blue=0 }, // Right Shift
+        { .red=57, .green=73, .blue=89 }, // N
+        { .red=58, .green=74, .blue=90 }, // M
+        { .red=59, .green=75, .blue=91 }, // ,
+        { .red=60, .green=76, .blue=92 }, // .
+        { .red=56, .green=72, .blue=88 }, // /
+        { .red=99, .green=115, .blue=131 }, // Right Shift
         { .red=0, .green=0, .blue=0 }, // Unused
 
         // Row 5
-        { .red=0, .green=0, .blue=0 }, // Right Space
-        { .red=0, .green=0, .blue=0 }, // Right Mod
-        { .red=0, .green=0, .blue=0 }, // Right Fn
-        { .red=0, .green=0, .blue=0 }, // Right Alt
-        { .red=0, .green=0, .blue=0 }, // Right Super
-        { .red=0, .green=0, .blue=0 }, // Right Control
+        { .red=105, .green=121, .blue=137 }, // Right Space
+        { .red=0, .green=0, .blue=0 }, // Right Mod (no backlight)
+        { .red=106, .green=122, .blue=138 }, // Right Fn
+        { .red=104, .green=120, .blue=136 }, // Right Alt
+        { .red=100, .green=116, .blue=132 }, // Right Super
+        { .red=101, .green=117, .blue=133 }, // Right Control
     },
 
     // Left keyboard half
