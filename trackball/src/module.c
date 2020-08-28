5bde6a507158335d747da1854f7835f65a10d762
László Monda <laci@monda.hu>
2020-08-28 21:28:51 +0200

    Fix trackball right button port.

--- trackball/src/module.c
+++ trackball/src/module.c
@@ -42,7 +42,7 @@ key_vector_t keyVector = {
     .itemNum = KEYBOARD_VECTOR_ITEMS_NUM,
     .items = (key_vector_pin_t[]) {
         {PORTA, GPIOA, kCLOCK_PortA, 3}, // left button
-        {PORTA, GPIOA, kCLOCK_PortA, 5}, // right button
+        {PORTA, GPIOA, kCLOCK_PortA, 12}, // right button
     },
 };
 
