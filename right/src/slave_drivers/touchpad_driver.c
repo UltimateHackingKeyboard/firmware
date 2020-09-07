277bb8dd502cc2372b4317cae932dc8adac0c37e
László Monda <laci@monda.hu>
2020-09-07 09:28:43 +0200

    Read the two touchpad event bytes with a single read operation.

--- right/src/slave_drivers/touchpad_driver.c
+++ right/src/slave_drivers/touchpad_driver.c
@@ -15,27 +15,26 @@ void TouchpadDriver_Init(uint8_t uhkModuleDriverId)
 }
 
 typedef struct {
-    bool singleTap: 1;
-    bool tapAndHold: 1;
-    uint8_t unused: 6;
-} gesture_events0_t;
+    struct {
+        bool singleTap: 1;
+        bool tapAndHold: 1;
+        uint8_t unused: 6;
+    } events0;
+    struct {
+        bool twoFingerTap : 1;
+        bool scroll : 1;
+        bool zoom : 1;
+        uint8_t unused: 5;
+    } events1;
+} gesture_events_t;
 
-typedef struct {
-    bool twoFingerTap : 1;
-    bool scroll : 1;
-    bool zoom : 1;
-    uint8_t unused: 5;
-} gesture_events1_t;
-
-static gesture_events0_t gestureEvents0;
-static gesture_events1_t gestureEvents1;
+static gesture_events_t gestureEvents;
 
 uint8_t address = I2C_ADDRESS_RIGHT_IQS5XX_FIRMWARE;
 touchpad_events_t TouchpadEvents;
 uint8_t phase = 0;
 static uint8_t enableEventMode[] = {0x05, 0x8f, 0x07};
 static uint8_t getGestureEvents0[] = {0x00, 0x0d};
-static uint8_t getGestureEvents1[] = {0x00, 0x0e};
 static uint8_t getRelativePixelsXCommand[] = {0x00, 0x12};
 static uint8_t getRelativePixelsYCommand[] = {0x00, 0x14};
 static uint8_t closeCommunicationWindow[] = {0xee, 0xee, 0xee};
@@ -59,51 +58,40 @@ status_t TouchpadDriver_Update(uint8_t uhkModuleDriverId)
             break;
         }
         case 2: {
-            status = I2cAsyncRead(address, (uint8_t*)&gestureEvents0, 1);
+            status = I2cAsyncRead(address, (uint8_t*)&gestureEvents, sizeof(gesture_events_t));
             phase = 3;
             break;
         }
         case 3: {
-            status = I2cAsyncWrite(address, getGestureEvents1, sizeof(getGestureEvents1));
+            status = I2cAsyncWrite(address, getRelativePixelsXCommand, sizeof(getRelativePixelsXCommand));
             phase = 4;
             break;
         }
         case 4: {
-            status = I2cAsyncRead(address, (uint8_t*)&gestureEvents1, 1);
+            status = I2cAsyncRead(address, buffer, 2);
+            deltaX = (int16_t)(buffer[1] | buffer[0]<<8);
             phase = 5;
             break;
         }
         case 5: {
-            status = I2cAsyncWrite(address, getRelativePixelsXCommand, sizeof(getRelativePixelsXCommand));
+            status = I2cAsyncWrite(address, getRelativePixelsYCommand, sizeof(getRelativePixelsYCommand));
             phase = 6;
             break;
         }
         case 6: {
             status = I2cAsyncRead(address, buffer, 2);
-            deltaX = (int16_t)(buffer[1] | buffer[0]<<8);
+            deltaY = (int16_t)(buffer[1] | buffer[0]<<8);
             phase = 7;
             break;
         }
         case 7: {
-            status = I2cAsyncWrite(address, getRelativePixelsYCommand, sizeof(getRelativePixelsYCommand));
-            phase = 8;
-            break;
-        }
-        case 8: {
-            status = I2cAsyncRead(address, buffer, 2);
-            deltaY = (int16_t)(buffer[1] | buffer[0]<<8);
-            phase = 9;
-            break;
-        }
-        case 9: {
-            TouchpadEvents.singleTap |= gestureEvents0.singleTap;
-            TouchpadEvents.twoFingerTap |= gestureEvents1.twoFingerTap;
-            //TouchpadEvents.tapAndHold = gestureEvents0.tapAndHold;
+            TouchpadEvents.singleTap |= gestureEvents.events0.singleTap;
+            TouchpadEvents.twoFingerTap |= gestureEvents.events1.twoFingerTap;
 
-            if (gestureEvents1.scroll) {
+            if (gestureEvents.events1.scroll) {
                 TouchpadEvents.wheelX -= deltaX;
                 TouchpadEvents.wheelY += deltaY;
-            } else if (gestureEvents1.zoom) {
+            } else if (gestureEvents.events1.zoom) {
                 TouchpadEvents.zoomLevel += deltaY;
             } else {
                 TouchpadEvents.x -= deltaX;
