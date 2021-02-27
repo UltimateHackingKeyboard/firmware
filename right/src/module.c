298ac146b44bd6ca50a40ece82229460f6734089
László Monda <laci@monda.hu>
2021-02-27 14:25:13 +0100

    Add default module navigation modes.

--- right/src/module.c
+++ right/src/module.c
@@ -1,3 +1,28 @@
 #include "module.h"
 
-navigation_mode_t ModuleNavigationModes[ModuleId_Count][LayerId_Count];
+navigation_mode_t ModuleNavigationModes[ModuleId_Count][LayerId_Count] = {
+    { // ModuleId_KeyClusterLeft
+        NavigationMode_Scroll, // Base layer
+        NavigationMode_Caret, // Mod layer
+        NavigationMode_Media, // Fn layer
+        NavigationMode_Cursor, // Mouse layer
+    },
+    { // ModuleId_TrackballRight
+        NavigationMode_Cursor, // Base layer
+        NavigationMode_Caret, // Mod layer
+        NavigationMode_Media, // Fn layer
+        NavigationMode_Scroll, // Mouse layer
+    },
+    { // ModuleId_TrackpointRight
+        NavigationMode_Cursor, // Base layer
+        NavigationMode_Caret, // Mod layer
+        NavigationMode_Media, // Fn layer
+        NavigationMode_Scroll, // Mouse layer
+    },
+    { // ModuleId_TouchpadRight
+        NavigationMode_Cursor, // Base layer
+        NavigationMode_Caret, // Mod layer
+        NavigationMode_Media, // Fn layer
+        NavigationMode_Scroll, // Mouse layer
+    }
+};
