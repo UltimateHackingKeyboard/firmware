8b6bdcea30ba5165ea14d609a88308bdcb5b6e63
steamraven <steamraven@users.noreply.github.com>
2021-03-15 03:34:06 -0600

    General ledmap & led_display formatting and fixes (#338)
    - Convert to hex to easily see patterns
    - Extra comments
    - Fix backspace ledmap
    - Update ledmap unused/zero detection
    
    Co-authored-by: M. Hawn <matthewh@steamraven.com>

--- right/src/ledmap.c
+++ right/src/ledmap.c
@@ -16,120 +16,120 @@ rgb_t KeyActionColors[] = {
 };
 
 rgb_t LedMap[SLOT_COUNT][MAX_KEY_COUNT_PER_MODULE] = {
+    // All three values must be set to 0 for unused
+    
     // Right keyboard half
     {
+        // IS31FL3737 has a weird memory layout for PWM values:
+        // 0x?0 - 0x?5 and  0x?8 - 0x?D
+        //   where ? is  0-B
         // Row 1
-        { .red=144, .green=160, .blue=32 }, // 7
-        { .red=145, .green=161, .blue=33 }, // 8
-        { .red=146, .green=162, .blue=34 }, // 9
-        { .red=147, .green=163, .blue=35 }, // 0
-        { .red=148, .green=164, .blue=36 }, // -
-        { .red=149, .green=165, .blue=37 }, // =
-        { .red=152, .green=168, .blue=0 }, // Backspace
-
+        { .red=0x90, .green=0xA0, .blue=0x20 }, // 7
+        { .red=0x91, .green=0xA1, .blue=0x21 }, // 8
+        { .red=0x92, .green=0xA2, .blue=0x22 }, // 9
+        { .red=0x93, .green=0xA3, .blue=0x23 }, // 0
+        { .red=0x94, .green=0xA4, .blue=0x24 }, // -
+        { .red=0x95, .green=0xA5, .blue=0x25 }, // =
+        { .red=0x98, .green=0xA8, .blue=0x28 }, // Backspace
         // Row 2
-        { .red=153, .green=169, .blue=41 }, // U
-        { .red=154, .green=170, .blue=42 }, // I
-        { .red=155, .green=171, .blue=43 }, // O
-        { .red=156, .green=172, .blue=44 }, // P
-        { .red=96, .green=112, .blue=128 }, // [
-        { .red=97, .green=113, .blue=129 }, // ]
-        { .red=98, .green=114, .blue=130 }, // Backslash
-        { .red=157, .green=173, .blue=45 }, // Y
+        { .red=0x99, .green=0xA9, .blue=0x29 }, // U
+        { .red=0x9A, .green=0xAA, .blue=0x2A }, // I
+        { .red=0x9B, .green=0xAB, .blue=0x2B }, // O
+        { .red=0x9C, .green=0xAC, .blue=0x2C }, // P
+        { .red=0x60, .green=0x70, .blue=0x80 }, // [
+        { .red=0x61, .green=0x71, .blue=0x81 }, // ]
+        { .red=0x62, .green=0x72, .blue=0x82 }, // Backslash
+        { .red=0x9D, .green=0xAD, .blue=0x2D }, // Y
 
         // Row 3
-        { .red=48, .green=64, .blue=80 }, // J
-        { .red=49, .green=65, .blue=81 }, // K
-        { .red=50, .green=66, .blue=82 }, // L
-        { .red=51, .green=67, .blue=83 }, // ;
-        { .red=52, .green=68, .blue=84 }, // '
-        { .red=53, .green=69, .blue=85 }, // Enter
-        { .red=61, .green=77, .blue=93 }, // H
+        { .red=0x30, .green=0x40, .blue=0x50 }, // J
+        { .red=0x31, .green=0x41, .blue=0x51 }, // K
+        { .red=0x32, .green=0x42, .blue=0x52 }, // L
+        { .red=0x33, .green=0x43, .blue=0x53 }, // ;
+        { .red=0x34, .green=0x44, .blue=0x54 }, // '
+        { .red=0x35, .green=0x45, .blue=0x55 }, // Enter
+        { .red=0x3D, .green=0x4D, .blue=0x5D }, // H
 
         // Row 4
-        { .red=57, .green=73, .blue=89 }, // N
-        { .red=58, .green=74, .blue=90 }, // M
-        { .red=59, .green=75, .blue=91 }, // ,
-        { .red=60, .green=76, .blue=92 }, // .
-        { .red=56, .green=72, .blue=88 }, // /
-        { .red=99, .green=115, .blue=131 }, // Right Shift
-        { .red=0, .green=0, .blue=0 }, // Unused
+        { .red=0x39, .green=0x49, .blue=0x59 }, // N
+        { .red=0x3A, .green=0x4A, .blue=0x5A }, // M
+        { .red=0x3B, .green=0x4B, .blue=0x5B }, // ,
+        { .red=0x3C, .green=0x4C, .blue=0x5C }, // .
+        { .red=0x38, .green=0x48, .blue=0x58 }, // /
+        { .red=0x63, .green=0x73, .blue=0x83}, // Right Shift
+        { .red=0,    .green=0,    .blue=0   }, // Unused
 
         // Row 5
-        { .red=105, .green=121, .blue=137 }, // Right Space
-        { .red=0, .green=0, .blue=0 }, // Right Mod (no backlight)
-        { .red=106, .green=122, .blue=138 }, // Right Fn
-        { .red=104, .green=120, .blue=136 }, // Right Alt
-        { .red=100, .green=116, .blue=132 }, // Right Super
-        { .red=101, .green=117, .blue=133 }, // Right Control
+        { .red=0x69, .green=0x79, .blue=0x89 }, // Right Space
+        { .red=0,    .green=0,    .blue=0    }, // Right Mod (no backlight)
+        { .red=0x6A, .green=0x7A, .blue=0x8A }, // Right Fn
+        { .red=0x68, .green=0x78, .blue=0x88 }, // Right Alt
+        { .red=0x64, .green=0x74, .blue=0x84 }, // Right Super
+        { .red=0x65, .green=0x75, .blue=0x85 }, // Right Control
     },
 
     // Left keyboard half
     {
+        // IS31FL3737 has a weird memory layout for PWM values:
+        // 0x?0 - 0x?5 and  0x?8 - 0x?D
+        //   where ? is  0-B        
         // Row 1
-        { .red=106, .green=122, .blue=138 }, // `
-        { .red=107, .green=123, .blue=139 }, // 1
-        { .red=108, .green=124, .blue=140 }, // 2
-        { .red=154, .green=170, .blue=186 }, // 3
-        { .red=155, .green=171, .blue=187 }, // 4
-        { .red=156, .green=172, .blue=188 }, // 5
-        { .red=157, .green=173, .blue=189 }, // 6
+        { .red=0x6A, .green=0x7A, .blue=0x8A }, // `
+        { .red=0x6B, .green=0x7B, .blue=0x8B }, // 1
+        { .red=0x6C, .green=0x7C, .blue=0x8C }, // 2
+        { .red=0x9A, .green=0xAA, .blue=0xBA }, // 3
+        { .red=0x9B, .green=0xAB, .blue=0xBB }, // 4
+        { .red=0x9C, .green=0xAC, .blue=0xBC }, // 5
+        { .red=0x9D, .green=0xAD, .blue=0xBD }, // 6
 
         // Row 2
-        { .red=0, .green=16, .blue=32 }, // Tab
-        { .red=2, .green=18, .blue=34 }, // Q
-        { .red=4, .green=20, .blue=36 }, // W
-        { .red=8, .green=24, .blue=40 }, // E
-        { .red=10, .green=26, .blue=42 }, // R
-        { .red=0, .green=0, .blue=0 }, // Unused
-        { .red=12, .green=28, .blue=44 }, // T
+        { .red=0x00, .green=0x10, .blue=0x20 }, // Tab
+        { .red=0x02, .green=0x12, .blue=0x22 }, // Q
+        { .red=0x04, .green=0x14, .blue=0x24 }, // W
+        { .red=0x08, .green=0x18, .blue=0x28 }, // E
+        { .red=0x0A, .green=0x1A, .blue=0x2A }, // R
+        { .red=0,    .green=0,    .blue=0    }, // Unused
+        { .red=0x0C, .green=0x1C, .blue=0x2c }, // T
 
         // Row 3
-        { .red=1, .green=17, .blue=33 }, // Mouse
-        { .red=3, .green=19, .blue=35 }, // A
-        { .red=5, .green=21, .blue=37 }, // S
-        { .red=9, .green=25, .blue=41 }, // D
-        { .red=11, .green=27, .blue=43 }, // F
-        { .red=0, .green=0, .blue=0 }, // Unused
-        { .red=13, .green=29, .blue=45 }, // G
+        { .red=0x01, .green=0x11, .blue=0x21 }, // Mouse
+        { .red=0x03, .green=0x13, .blue=0x23 }, // A
+        { .red=0x05, .green=0x15, .blue=0x25 }, // S
+        { .red=0x09, .green=0x19, .blue=0x29 }, // D
+        { .red=0x0B, .green=0x1B, .blue=0x2B }, // F
+        { .red=0,    .green=0,    .blue=0    }, // Unused
+        { .red=0x0D, .green=0x1D, .blue=0x2D }, // G
 
         // Row 4
-        { .red=48, .green=64, .blue=80 }, // ANSI Left Shift
-//        { .red=109, .green=125, .blue=141 }, // ISO Left Shift
-        { .red=50, .green=66, .blue=82 }, // ISO Key
-        { .red=52, .green=68, .blue=84 }, // Z
-        { .red=56, .green=72, .blue=88 }, // X
-        { .red=58, .green=74, .blue=90 }, // C
-        { .red=59, .green=75, .blue=91 }, // V
-        { .red=61, .green=77, .blue=93 }, // B
+        { .red=0x30, .green=0x40, .blue=0x50 }, // ANSI Left Shift
+//        { .red=0x6D, .green=0x7D, .blue=0x8D }, // ISO Left Shift
+        { .red=0x32, .green=0x42, .blue=0x52 }, // ISO Key
+        { .red=0x34, .green=0x44, .blue=0x54 }, // Z
+        { .red=0x38, .green=0x48, .blue=0x58 }, // X
+        { .red=0x3A, .green=0x4A, .blue=0x5A }, // C
+        { .red=0x3B, .green=0x4B, .blue=0x5B }, // V
+        { .red=0x3D, .green=0x4D, .blue=0x5D }, // B
 
         // Row 5
-        { .red=49, .green=65, .blue=81 }, // Left Control
-        { .red=51, .green=67, .blue=83 }, // Left Super
-        { .red=53, .green=69, .blue=85 }, // Left Alt
-        { .red=57, .green=73, .blue=89 }, // Left Fn
-        { .red=0, .green=0, .blue=0 }, // Left Space (no backlight)
-        { .red=60, .green=76, .blue=92 }, // Left Mod
-        { .red=0, .green=0, .blue=0 } // Unused
+        { .red=0x31, .green=0x41, .blue=0x51 }, // Left Control
+        { .red=0x33, .green=0x43, .blue=0x53 }, // Left Super
+        { .red=0x35, .green=0x45, .blue=0x55 }, // Left Alt
+        { .red=0x39, .green=0x49, .blue=0x59 }, // Left Fn
+        { .red=0,    .green=0,    .blue=0    }, // Left Space (no backlight)
+        { .red=0x3C, .green=0x4C, .blue=0x5C }, // Left Mod
+        { .red=0,    .green=0,    .blue=0 } // Unused
     },
 
     // Left module
     {
-        { .red=5, .green=4, .blue=3 },
-        { .red=2, .green=1, .blue=0 },
-        { .red=7, .green=6, .blue=8 },
+        { .red=0x05, .green=0x04, .blue=0x03 },
+        { .red=0x02, .green=0x01, .blue=0x00 },
+        { .red=0x07, .green=0x06, .blue=0x08 },
     },
 
     // Right module
     {
     },
-};
-
-uint8_t validZeroKeyIds[SLOT_COUNT] = {
-    6, // Right keyboard half
-    7, // Left keyboard half
-    1, // Left module
-    0, // Right module
 };
 
 void UpdateLayerLeds(void) {
@@ -169,16 +169,13 @@ void UpdateLayerLeds(void) {
 
             rgb_t *keyActionColorValues = &KeyActionColors[keyActionColor];
             rgb_t *ledMapItem = &LedMap[slotId][keyId];
-            if (ledMapItem->red != 0 || (ledMapItem->red == 0 && validZeroKeyIds[slotId] == keyId)) {
-                LedDriverValues[slotId][ledMapItem->red] = keyActionColorValues->red * KeyBacklightBrightness / 255;
+            if (ledMapItem->red == 0 && ledMapItem->green == 0 && ledMapItem->blue == 0) {
+                continue;
             }
-            if (ledMapItem->green != 0 || (ledMapItem->green== 0 && validZeroKeyIds[slotId] == keyId)) {
-                float brightnessDivisor = slotId == SlotId_LeftModule ? 2 : 1;
-                LedDriverValues[slotId][ledMapItem->green] = keyActionColorValues->green * KeyBacklightBrightness / brightnessDivisor / 255;
-            }
-            if (ledMapItem->blue != 0 || (ledMapItem->blue == 0 && validZeroKeyIds[slotId] == keyId)) {
-                LedDriverValues[slotId][ledMapItem->blue] = keyActionColorValues->blue * KeyBacklightBrightness / 255;
-            }
+            LedDriverValues[slotId][ledMapItem->red] = keyActionColorValues->red * KeyBacklightBrightness / 255;
+            float brightnessDivisor = slotId == SlotId_LeftModule ? 2 : 1;
+            LedDriverValues[slotId][ledMapItem->green] = keyActionColorValues->green * KeyBacklightBrightness / brightnessDivisor / 255;
+            LedDriverValues[slotId][ledMapItem->blue] = keyActionColorValues->blue * KeyBacklightBrightness / 255;
         }
     }
 #endif
