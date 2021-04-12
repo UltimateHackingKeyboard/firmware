f3d60077a01c1ffbeafa4c995eb79c15e587ac4b
Karel Tuček <kareltucek@centrum.cz>
2021-04-12 16:57:35 +0200

    Add some more documentation to secondary roles.

--- right/src/secondary_role_driver.c
+++ right/src/secondary_role_driver.c
@@ -4,6 +4,52 @@
 #include "timer.h"
 #include "math.h"
 
+/*
+ * ## Strategies:
+ *
+ * - simple
+ * - timeout (advanced alphanumeric-friendly)
+ *
+ * ## Simple strategy:
+ *
+ * Simple strategy works simply with press events (i.e., keyDown events). If there is any action pressed when a dual-role key is held, the secondary role goes to secondary role mode. Otherwise, it produces its primary action on its release event.
+ *
+ * ## Timeout strategy:
+ *
+ * Timeout strategy works mainly with key releases:
+ *
+ * - Secondary role is activated if timeout (i.e., triggerTime) is reached.
+ * - Secondary role is activated if action key is pressed and released within the span of press of the dual role key. (If allowTriggerByRelease is active.)
+ * - Long hold of primary action can be achieved by doubletapping the key. (If doubletapActivatedPrimary is active.)
+ *
+ * Configuration params:
+ *
+ * - DoubletapTime - if dual-role key is tapped and press within this time (press-to-press), then primary role is activated.
+ * - TriggerTime configures the basic timeout of the dual-role key.
+ * - TimeoutAction - after triggerTime, the key is defaulted to timeoutAction. Typically to secondary role.
+ * - SafetyMargin decreases probability of unintentional secondary role, by offsetting release time of the keys by the amount.
+ * - AllowTriggerByRelease - if set to false, the only way to trigger secondary role is to press it for more than triggerTime.
+ * - DoubletapActivatesPrimary - if set to false, doubletap logic is disabled. Useful if you prefer tap and hold evaluate to primary role.
+ *
+ * This yields two most basic activation modes:
+ *
+ * - Via timeout. Triggered via distinct and slightly prolonged press of the shortcut.
+ *   - Pros: works with short timeout.
+ *   - Cons: some typing styles may cause unwanted activations of secondary role.
+ *   - Recommended params:
+ *     - TriggerTime = 200
+ *     - SafetyMargin = 0
+ *     - AllowTriggerByRelease = false
+ *
+ * - Via release order. Triggered mainly by correct release sequence.
+ *   - Pros: is more reliable in suppressing unwanted secondary role activations.
+ *   - Cons: activation might require a bit longer hold.
+ *   - Recommended params:
+ *     - TriggerTime = 350
+ *     - SafetyMargin = 50
+ *     - AllowTriggerByRelease = true
+ */
+
 static key_state_t* resolutionKey;
 static secondary_role_state_t resolutionState;
 static uint32_t resolutionStartTime;
@@ -51,39 +97,47 @@ uint16_t timeoutStrategyTriggerTime = 350;
 uint16_t timeoutStrategySafetyMargin = 50;
 bool timeoutStrategyAllowTriggerByRelease = true;
 bool timeoutStrategyDoubletapActivatesPrimary = true;
-secondary_role_state_t timeoutStrategyTimeoutAction = SecondaryRoleState_Primary;
+secondary_role_state_t timeoutStrategyTimeoutAction = SecondaryRoleState_Secondary;
 
 static secondary_role_state_t resolveCurrentKeyRoleIfDontKnowTimeout()
 {
     //gather data
     uint32_t dualRolePressTime = resolutionStartTime;
-    struct postponer_buffer_record_type_t* dummy;
-    struct postponer_buffer_record_type_t* dualRoleRelease;
-    struct postponer_buffer_record_type_t* actionPress;
-    struct postponer_buffer_record_type_t* actionRelease;
+    struct postponer_buffer_record_type_t *dummy;
+    struct postponer_buffer_record_type_t *dualRoleRelease;
+    struct postponer_buffer_record_type_t *actionPress;
+    struct postponer_buffer_record_type_t *actionRelease;
 
     PostponerQuery_InfoByKeystate(resolutionKey, &dummy, &dualRoleRelease);
     PostponerQuery_InfoByQueueIdx(0, &actionPress, &actionRelease);
 
     //handle doubletap logic
-    if(
-            timeoutStrategyDoubletapActivatesPrimary
-            && resolutionKey == previousResolutionKey
-            && resolutionStartTime - previousResolutionTime < timeoutStrategyDoubletapTime
-    ) {
-        return timeoutStrategyTimeoutAction;
+    if (
+        timeoutStrategyDoubletapActivatesPrimary
+        && resolutionKey == previousResolutionKey
+        && resolutionStartTime - previousResolutionTime < timeoutStrategyDoubletapTime) {
+        activatePrimary();
+        return SecondaryRoleState_Primary;
     }
 
     //action key has not been pressed yet -> timeout scenarios
-    if(actionPress == NULL) {
-        if(dualRoleRelease != NULL) {
+    if (actionPress == NULL) {
+        if (dualRoleRelease != NULL) {
             activatePrimary();
             return SecondaryRoleState_Primary;
-        } else if(CurrentTime - dualRolePressTime > timeoutStrategyTriggerTime) {
-            activatePrimary();
-            return SecondaryRoleState_Primary;
+        } else if (CurrentTime - dualRolePressTime > timeoutStrategyTriggerTime) {
+            switch (timeoutStrategyTimeoutAction) {
+            case SecondaryRoleState_Primary:
+                activatePrimary();
+                return SecondaryRoleState_Primary;
+            case SecondaryRoleState_Secondary:
+                activateSecondary();
+                return SecondaryRoleState_Secondary;
+            default:
+                return SecondaryRoleState_DontKnowYet;
+            }
         } else {
-        	return SecondaryRoleState_DontKnowYet;
+            return SecondaryRoleState_DontKnowYet;
         }
     }
 
