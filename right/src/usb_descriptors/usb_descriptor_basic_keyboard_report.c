383ddd4d76a8b359abdbaad7fca2dad6c305df0f
Benedek Kupper <kupper.benedek@gmail.com>
2021-12-02 23:00:25 +0100

    report descriptors: general readability improvements
    * use decimal numbers where amounts are expressed
    * remove HID_IOF_NON_VOLATILE as it's a 0 flag
    * use existing enum values to explain some magic numbers
    
    Signed-off-by: Benedek Kupper <kupper.benedek@gmail.com>

--- right/src/usb_descriptors/usb_descriptor_basic_keyboard_report.c
+++ right/src/usb_descriptors/usb_descriptor_basic_keyboard_report.c
@@ -1,5 +1,6 @@
 #include "usb_api.h"
 #include "usb_descriptor_basic_keyboard_report.h"
+#include "lufa/HIDClassCommon.h"
 
 uint8_t UsbBasicKeyboardReportDescriptor[USB_BASIC_KEYBOARD_REPORT_DESCRIPTOR_LENGTH] = {
     HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_GENERIC_DESKTOP),
@@ -8,17 +9,17 @@ uint8_t UsbBasicKeyboardReportDescriptor[USB_BASIC_KEYBOARD_REPORT_DESCRIPTOR_LE
 
         // Modifiers
         HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_KEY_CODES),
-        HID_RI_USAGE_MINIMUM(8, 0xE0),
-        HID_RI_USAGE_MAXIMUM(8, 0xE7),
-        HID_RI_LOGICAL_MINIMUM(8, 0x00),
-        HID_RI_LOGICAL_MAXIMUM(8, 0x01),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_REPORT_COUNT(8, 0x08),
+        HID_RI_USAGE_MINIMUM(8, HID_KEYBOARD_SC_LEFT_CONTROL),
+        HID_RI_USAGE_MAXIMUM(8, HID_KEYBOARD_SC_RIGHT_GUI),
+        HID_RI_LOGICAL_MINIMUM(8, 0),
+        HID_RI_LOGICAL_MAXIMUM(8, 1),
+        HID_RI_REPORT_SIZE(8, 1),
+        HID_RI_REPORT_COUNT(8, 8),
         HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
 
         // Reserved for future use, always 0
-        HID_RI_REPORT_COUNT(8, 0x01),
-        HID_RI_REPORT_SIZE(8, 0x08),
+        HID_RI_REPORT_COUNT(8, 1),
+        HID_RI_REPORT_SIZE(8, 8),
         HID_RI_INPUT(8, HID_IOF_CONSTANT),
 
         // Scancodes
@@ -28,20 +29,20 @@ uint8_t UsbBasicKeyboardReportDescriptor[USB_BASIC_KEYBOARD_REPORT_DESCRIPTOR_LE
         HID_RI_USAGE_MINIMUM(8, 0x00),
         HID_RI_USAGE_MAXIMUM(8, 0xFF),
         HID_RI_REPORT_COUNT(8, USB_BASIC_KEYBOARD_MAX_KEYS),
-        HID_RI_REPORT_SIZE(8, 0x08),
+        HID_RI_REPORT_SIZE(8, 8),
         HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
 
         // LED status - Num lock, Caps lock, Scroll lock, Compose, Kana
         HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_LEDS),
-        HID_RI_USAGE_MINIMUM(8, 0x01),
-        HID_RI_USAGE_MAXIMUM(8, 0x05),
-        HID_RI_REPORT_COUNT(8, 0x05),
-        HID_RI_REPORT_SIZE(8, 0x01),
-        HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE | HID_IOF_NON_VOLATILE),
+        HID_RI_USAGE_MINIMUM(8, HID_LED_UID_NUMLOCK),
+        HID_RI_USAGE_MAXIMUM(8, HID_LED_UID_KANA),
+        HID_RI_REPORT_COUNT(8, 5),
+        HID_RI_REPORT_SIZE(8, 1),
+        HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
 
         // LED status padding
-        HID_RI_REPORT_COUNT(8, 0x01),
-        HID_RI_REPORT_SIZE(8, 0x03),
+        HID_RI_REPORT_COUNT(8, 1),
+        HID_RI_REPORT_SIZE(8, 3),
         HID_RI_OUTPUT(8, HID_IOF_CONSTANT),
 
     HID_RI_END_COLLECTION(0),
