554efa9337f417630dfc6d5d6e50b5bab67bdc21
Benedek Kupper <kupper.benedek@gmail.com>
2021-12-15 23:57:40 +0100

    Don't provide idle repeat reports (#390)
    For justification please see https://github.com/UltimateHackingKeyboard/firmware/issues/390#issuecomment-991797166

--- right/src/usb_interfaces/usb_interface_generic_hid.c
+++ right/src/usb_interfaces/usb_interface_generic_hid.c
@@ -1,11 +1,9 @@
 #include "usb_composite_device.h"
 #include "usb_protocol_handler.h"
-#include "timer.h"
 
 uint32_t UsbGenericHidActionCounter;
 uint8_t GenericHidOutBuffer[USB_GENERIC_HID_OUT_BUFFER_LENGTH];
 uint8_t GenericHidInBuffer[USB_GENERIC_HID_IN_BUFFER_LENGTH];
-static uint32_t usbGenericHidReportLastSendTime = 0;
 
 static usb_status_t UsbReceiveData(void)
 {
@@ -21,13 +19,7 @@ static usb_status_t UsbReceiveData(void)
 
 usb_status_t UsbGenericHidCheckIdleElapsed()
 {
-    uint16_t idlePeriodMs = ((usb_device_hid_struct_t*)UsbCompositeDevice.genericHidHandle)->idleRate * 4; // idleRate is in 4ms units.
-    if (!idlePeriodMs) {
-        return kStatus_USB_Busy;
-    }
-
-    bool hasIdleElapsed = (Timer_GetElapsedTimeMicros(&usbGenericHidReportLastSendTime) / 1000) > idlePeriodMs;
-    return hasIdleElapsed ? kStatus_USB_Success : kStatus_USB_Busy;
+    return kStatus_USB_Busy;
 }
 
 usb_status_t UsbGenericHidCheckReportReady()
@@ -79,11 +71,6 @@ usb_status_t UsbGenericHidCallback(class_handle_t handle, uint32_t event, void *
             }
             break;
         }
-
-        case kUSB_DeviceHidEventSetIdle:
-            usbGenericHidReportLastSendTime = CurrentTime;
-            error = kStatus_USB_Success;
-            break;
 
         default:
             break;
