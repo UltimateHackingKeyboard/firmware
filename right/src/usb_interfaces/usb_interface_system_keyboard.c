778deefa73c0b8865285b2c3ab049d7153dba538
Benedek Kupper <kupper.benedek@gmail.com>
2021-12-15 23:43:14 +0100

    fixes for HID class requests
    So far there was a mix of using the handle's variables as buffer (protocol),
    and as actual value storage (idleRate). Let's decide to store the protocol
    also in the handle instead of in separate variable. This requires the
    proper reset of these values at each SET_CONFIGURATION. This also means
    that the GET_IDLE and GET_PROTOCOL requests don't need to be processed by
    the interfaces.
    
    Add the missing success flags to the callback paths where the request is
    accepted. Remove empty cases. Only call back if the descriptor indicates
    BOOT subclass.

--- right/src/usb_interfaces/usb_interface_system_keyboard.c
+++ right/src/usb_interfaces/usb_interface_system_keyboard.c
@@ -59,7 +59,7 @@ usb_status_t UsbSystemKeyboardCheckReportReady()
 
 usb_status_t UsbSystemKeyboardCallback(class_handle_t handle, uint32_t event, void *param)
 {
-    usb_status_t error = kStatus_USB_Error;
+    usb_status_t error = kStatus_USB_InvalidRequest;
 
     switch (event) {
         case kUSB_DeviceHidEventSendResponse:
@@ -67,9 +67,6 @@ usb_status_t UsbSystemKeyboardCallback(class_handle_t handle, uint32_t event, vo
             if (UsbCompositeDevice.attach) {
                 error = kStatus_USB_Success;
             }
-            break;
-        case kUSB_DeviceHidEventRecvResponse:
-            error = kStatus_USB_InvalidRequest;
             break;
 
         case kUSB_DeviceHidEventGetReport: {
@@ -84,25 +81,9 @@ usb_status_t UsbSystemKeyboardCallback(class_handle_t handle, uint32_t event, vo
             break;
         }
 
-        // SetReport is not required for this interface.
-        case kUSB_DeviceHidEventSetReport:
-        case kUSB_DeviceHidEventRequestReportBuffer:
-            error = kStatus_USB_InvalidRequest;
-            break;
-
-        case kUSB_DeviceHidEventGetIdle:
-            error = kStatus_USB_Success;
-            break;
-
         case kUSB_DeviceHidEventSetIdle:
             usbSystemKeyboardReportLastSendTime = CurrentTime;
             error = kStatus_USB_Success;
-            break;
-
-        // No boot protocol support for this interface.
-        case kUSB_DeviceHidEventGetProtocol:
-        case kUSB_DeviceHidEventSetProtocol:
-            error = kStatus_USB_InvalidRequest;
             break;
 
         default:
