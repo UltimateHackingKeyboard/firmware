287849056db7a538b2324d5ef5d0cea738764441
Benedek Kupper <kupper.benedek@gmail.com>
2022-02-15 17:36:22 +0100

    move report descriptors inline into header
    The purpose of this change is to be able to modify the report descriptors
    inline, without having to separately adjust the hardcoded length.
    By using static keyword, the descriptor will be copied into every translation unit,
    but this isn't a problem even without smart compilers, because these
    descriptors are only used once, even if their size is used in multiple places.

--- right/src/usb_descriptors/usb_descriptor_basic_keyboard_report.h
+++ right/src/usb_descriptors/usb_descriptor_basic_keyboard_report.h
@@ -1,13 +1,62 @@
 #ifndef __USB_DESCRIPTOR_BASIC_KEYBOARD_REPORT_H__
 #define __USB_DESCRIPTOR_BASIC_KEYBOARD_REPORT_H__
 
+// Includes:
+
+    #include "usb_api.h"
+    #include "lufa/HIDClassCommon.h"
+
 // Macros:
 
-    #define USB_BASIC_KEYBOARD_REPORT_DESCRIPTOR_LENGTH 64
+    #define USB_BASIC_KEYBOARD_REPORT_DESCRIPTOR_LENGTH (sizeof(UsbBasicKeyboardReportDescriptor))
     #define USB_BASIC_KEYBOARD_MAX_KEYS 6
 
 // Variables:
 
-    extern USB_DESC_STORAGE_TYPE UsbBasicKeyboardReportDescriptor[USB_BASIC_KEYBOARD_REPORT_DESCRIPTOR_LENGTH];
+    static USB_DESC_STORAGE_TYPE UsbBasicKeyboardReportDescriptor[] = {
+        HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_GENERIC_DESKTOP),
+        HID_RI_USAGE(8, HID_RI_USAGE_GENERIC_DESKTOP_KEYBOARD),
+        HID_RI_COLLECTION(8, HID_RI_COLLECTION_APPLICATION),
+
+            // Modifiers
+            HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_KEY_CODES),
+            HID_RI_USAGE_MINIMUM(8, HID_KEYBOARD_SC_LEFT_CONTROL),
+            HID_RI_USAGE_MAXIMUM(8, HID_KEYBOARD_SC_RIGHT_GUI),
+            HID_RI_LOGICAL_MINIMUM(8, 0),
+            HID_RI_LOGICAL_MAXIMUM(8, 1),
+            HID_RI_REPORT_SIZE(8, 1),
+            HID_RI_REPORT_COUNT(8, 8),
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+
+            // Reserved for future use, always 0
+            HID_RI_REPORT_COUNT(8, 1),
+            HID_RI_REPORT_SIZE(8, 8),
+            HID_RI_INPUT(8, HID_IOF_CONSTANT),
+
+            // Scancodes
+            HID_RI_LOGICAL_MINIMUM(8, 0x00),
+            HID_RI_LOGICAL_MAXIMUM(16, 0xFF),
+            HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_KEY_CODES),
+            HID_RI_USAGE_MINIMUM(8, 0x00),
+            HID_RI_USAGE_MAXIMUM(8, 0xFF),
+            HID_RI_REPORT_COUNT(8, USB_BASIC_KEYBOARD_MAX_KEYS),
+            HID_RI_REPORT_SIZE(8, 8),
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_ARRAY | HID_IOF_ABSOLUTE),
+
+            // LED status - Num lock, Caps lock, Scroll lock, Compose, Kana
+            HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_LEDS),
+            HID_RI_USAGE_MINIMUM(8, HID_LED_UID_NUMLOCK),
+            HID_RI_USAGE_MAXIMUM(8, HID_LED_UID_KANA),
+            HID_RI_REPORT_COUNT(8, 5),
+            HID_RI_REPORT_SIZE(8, 1),
+            HID_RI_OUTPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+
+            // LED status padding
+            HID_RI_REPORT_COUNT(8, 1),
+            HID_RI_REPORT_SIZE(8, 3),
+            HID_RI_OUTPUT(8, HID_IOF_CONSTANT),
+
+        HID_RI_END_COLLECTION(0),
+    };
 
 #endif
--- right/src/usb_descriptors/usb_descriptor_system_keyboard_report.h
+++ right/src/usb_descriptors/usb_descriptor_system_keyboard_report.h
@@ -1,13 +1,36 @@
 #ifndef __USB_DESCRIPTOR_SYSTEM_KEYBOARD_REPORT_H__
 #define __USB_DESCRIPTOR_SYSTEM_KEYBOARD_REPORT_H__
 
+// Includes:
+
+    #include "usb_api.h"
+    #include "arduino_hid/SystemAPI.h"
+
 // Macros:
 
-    #define USB_SYSTEM_KEYBOARD_REPORT_DESCRIPTOR_LENGTH 27
+    #define USB_SYSTEM_KEYBOARD_REPORT_DESCRIPTOR_LENGTH (sizeof(UsbSystemKeyboardReportDescriptor))
     #define USB_SYSTEM_KEYBOARD_MAX_KEYS 1
 
 // Variables:
 
-    extern USB_DESC_STORAGE_TYPE UsbSystemKeyboardReportDescriptor[USB_SYSTEM_KEYBOARD_REPORT_DESCRIPTOR_LENGTH];
+    static USB_DESC_STORAGE_TYPE UsbSystemKeyboardReportDescriptor[] = {
+        HID_RI_USAGE_PAGE(8, HID_RI_USAGE_PAGE_GENERIC_DESKTOP),
+        HID_RI_USAGE(8, HID_RI_USAGE_GENERIC_DESKTOP_SYSTEM_CONTROL),
+        HID_RI_COLLECTION(8, HID_RI_COLLECTION_APPLICATION),
+            // System keys
+            HID_RI_LOGICAL_MINIMUM(8, 0),
+            HID_RI_LOGICAL_MAXIMUM(8, 1),
+            HID_RI_USAGE_MINIMUM(8, SYSTEM_POWER_DOWN), // SYSTEM_POWER_DOWN, SYSTEM_SLEEP and SYSTEM_WAKE_UP
+            HID_RI_USAGE_MAXIMUM(8, SYSTEM_WAKE_UP),
+            HID_RI_REPORT_SIZE(8, 1),
+            HID_RI_REPORT_COUNT(8, 3),
+            HID_RI_INPUT(8, HID_IOF_DATA | HID_IOF_VARIABLE | HID_IOF_ABSOLUTE),
+
+            // Padding
+            HID_RI_REPORT_SIZE(8, 1),
+            HID_RI_REPORT_COUNT(8, 5),
+            HID_RI_INPUT(8, HID_IOF_CONSTANT),
+        HID_RI_END_COLLECTION(0),
+    };
 
 #endif
