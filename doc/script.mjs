c53c3f35180301a53b7e4bdd3d3fecce4001bf85
László Monda <laci@monda.hu>
2022-04-29 15:52:04 +0200

    Dump current slider value to the console.

--- doc/script.mjs
+++ doc/script.mjs
@@ -3,7 +3,12 @@ import {createApp} from './node_modules/vue/dist/vue.esm-browser.prod.js';
 // Components
 
 const Slider = {
-    template: `<input type="range">1.0`,
+    template: `<input type="range" ref="range" @input="onInput()">1.0`,
+    methods: {
+        onInput() {
+            console.log(this.$refs.range.value);
+        },
+    },
 };
 
 // Init Vue
