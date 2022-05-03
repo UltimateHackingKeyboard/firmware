a7084160169a40c46e1007dfaa5e37c87b650d94
László Monda <laci@monda.hu>
2022-05-03 21:35:00 +0200

    Finalize the test UI of the smart macro documentation.

--- doc/test.mjs
+++ doc/test.mjs
@@ -1,5 +1,22 @@
+import {createApp} from './node_modules/vue/dist/vue.esm-browser.prod.js';
 import Split from './node_modules/split.js/dist/split.es.js';
 
-Split(['#left', '#right'], {
-    sizes: [50, 50],
+const app = createApp({
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    mounted() {
+        Split(['#left', '#right'], {
+            sizes: [50, 50],
+        });
+    },
+    methods: {
+        change(target) {
+            console.log(target.target);
+        },
+    },
 });
+
+app.mount('body');
