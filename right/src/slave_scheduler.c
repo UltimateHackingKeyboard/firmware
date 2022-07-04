00a91d1799a68946fa7432517cb19d04f4a1d5ec
Karel Tuček <kareltucek@centrum.cz>
2022-07-04 15:52:59 +0200

    Properly disconnect slaves on i2c reinitialization.
    (uhk_module_state_t.moduleId is sometimes used as indicator that module is connected, so having slave.isConnected==false and moduleState->moduleId==0 *is* a problem. )

--- right/src/slave_scheduler.c
+++ right/src/slave_scheduler.c
@@ -9,6 +9,8 @@
 #include "i2c_addresses.h"
 #include "config.h"
 #include "i2c_error_logger.h"
+#include "macros.h"
+#include "debug.h"
 
 uint32_t I2cSlaveScheduler_Counter;
 
@@ -114,6 +116,9 @@ void InitSlaveScheduler(void)
 
     for (uint8_t i=0; i<SLAVE_COUNT; i++) {
         uhk_slave_t *currentSlave = Slaves + i;
+        if (currentSlave->isConnected && currentSlave->disconnect) {
+            currentSlave->disconnect(currentSlave->perDriverId);
+        }
         currentSlave->isConnected = false;
     }
 
