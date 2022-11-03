8ef4b6c4aeb35f7d6426954a2ba8fcf7020f0a4c
László Monda <laci@monda.hu>
2022-11-03 23:28:41 +0100

    Implement keyboard + mouse composite HID.

--- nordic-left/src/main.c
+++ nordic-left/src/main.c
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2018 qianfan Zhao
- * Copyright (c) 2018 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 #include <zephyr/zephyr.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
@@ -28,12 +22,9 @@ static const struct gpio_dt_spec sw0 = GPIO_SPEC(SW0_NODE),
 	sw3 = GPIO_SPEC(SW3_NODE),
 	led0 = GPIO_SPEC(LED0_NODE);
 
-static const uint8_t hid_report_desc[] = HID_MOUSE_REPORT_DESC(2);
+static const uint8_t hid_keyboard_report_desc[] = HID_KEYBOARD_REPORT_DESC();
+static const uint8_t hid_mouse_report_desc[] = HID_MOUSE_REPORT_DESC(2);
 
-static uint8_t def_val[4];
-static volatile uint8_t status[4];
-static K_SEM_DEFINE(sem, 0, 1);
-static struct gpio_callback callback[4];
 static enum usb_dc_status_code usb_status;
 
 #define MOUSE_BTN_REPORT_POS	0
@@ -44,141 +35,69 @@ static enum usb_dc_status_code usb_status;
 #define MOUSE_BTN_RIGHT		BIT(1)
 #define MOUSE_BTN_MIDDLE	BIT(2)
 
+uint8_t mouse_report[4] = { 0x00 };
+uint8_t keyboard_report[8] = { 0x00 };
+const struct device *hid_keyboard_dev;
+const struct device *hid_mouse_dev;
+
 static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
 {
+	LOG_DBG("usb %i", status);
 	usb_status = status;
+	if (status == USB_DC_CONFIGURED) {
+		hid_int_ep_write(hid_mouse_dev, mouse_report, sizeof(mouse_report), NULL);
+		hid_int_ep_write(hid_keyboard_dev, keyboard_report, sizeof(keyboard_report), NULL);
+	}
 }
 
-static void left_button(const struct device *gpio, struct gpio_callback *cb, uint32_t pins)
-{
-	int ret;
-	uint8_t state = status[MOUSE_BTN_REPORT_POS];
-
+/*
 	if (IS_ENABLED(CONFIG_USB_DEVICE_REMOTE_WAKEUP)) {
 		if (usb_status == USB_DC_SUSPEND) {
 			usb_wakeup_request();
 			return;
 		}
 	}
+*/
 
-	ret = gpio_pin_get(gpio, sw0.pin);
-
-	if (def_val[0] != (uint8_t)ret) {
-		state |= MOUSE_BTN_LEFT;
-	} else {
-		state &= ~MOUSE_BTN_LEFT;
-	}
-
-	if (status[MOUSE_BTN_REPORT_POS] != state) {
-		status[MOUSE_BTN_REPORT_POS] = state;
-		k_sem_give(&sem);
-	}
+void int_in_ready_mouse(const struct device *dev) {
+	mouse_report[MOUSE_X_REPORT_POS] = gpio_pin_get_dt(&sw0) ? 5 : 0;
+	mouse_report[MOUSE_Y_REPORT_POS] = gpio_pin_get_dt(&sw1) ? 5 : 0;
+	mouse_report[MOUSE_BTN_REPORT_POS] = gpio_pin_get_dt(&sw2) ? MOUSE_BTN_LEFT : 0;
+	hid_int_ep_write(hid_mouse_dev, mouse_report, sizeof(mouse_report), NULL);
 }
 
-static void right_button(const struct device *gpio, struct gpio_callback *cb,
-			 uint32_t pins)
-{
-	int ret;
-	uint8_t state = status[MOUSE_BTN_REPORT_POS];
-
-	if (IS_ENABLED(CONFIG_USB_DEVICE_REMOTE_WAKEUP)) {
-		if (usb_status == USB_DC_SUSPEND) {
-			usb_wakeup_request();
-			return;
-		}
-	}
-
-	ret = gpio_pin_get(gpio, sw1.pin);
-
-	if (def_val[1] != (uint8_t)ret) {
-		state |= MOUSE_BTN_RIGHT;
-	} else {
-		state &= ~MOUSE_BTN_RIGHT;
-	}
-
-	if (status[MOUSE_BTN_REPORT_POS] != state) {
-		status[MOUSE_BTN_REPORT_POS] = state;
-		k_sem_give(&sem);
-	}
-}
-
-static void x_move(const struct device *gpio, struct gpio_callback *cb,
-		   uint32_t pins)
-{
-	int ret;
-	uint8_t state = status[MOUSE_X_REPORT_POS];
-
-	ret = gpio_pin_get(gpio, sw2.pin);
-
-	if (def_val[2] != (uint8_t)ret) {
-		state += 10U;
-	}
-
-	if (status[MOUSE_X_REPORT_POS] != state) {
-		status[MOUSE_X_REPORT_POS] = state;
-		k_sem_give(&sem);
-	}
-}
-
-static void y_move(const struct device *gpio, struct gpio_callback *cb,
-		   uint32_t pins)
-{
-	int ret;
-	uint8_t state = status[MOUSE_Y_REPORT_POS];
-
-	ret = gpio_pin_get(gpio, sw3.pin);
-
-	if (def_val[3] != (uint8_t)ret) {
-		state += 10U;
-	}
-
-	if (status[MOUSE_Y_REPORT_POS] != state) {
-		status[MOUSE_Y_REPORT_POS] = state;
-		k_sem_give(&sem);
-	}
-}
-
-int callbacks_configure(const struct gpio_dt_spec *spec,
-			gpio_callback_handler_t handler,
-			struct gpio_callback *callback, uint8_t *val)
-{
-	const struct device *gpio = spec->port;
-	gpio_pin_t pin = spec->pin;
-
-	gpio_pin_configure_dt(spec, GPIO_INPUT);
-	gpio_pin_get(gpio, pin);
-	gpio_init_callback(callback, handler, BIT(pin));
-	gpio_add_callback(gpio, callback);
-	gpio_pin_interrupt_configure_dt(spec, GPIO_INT_EDGE_BOTH);
+void int_in_ready_keyboard(const struct device *dev) {
+	keyboard_report[2] = gpio_pin_get_dt(&sw3) ? HID_KEY_A : 0;
+	hid_int_ep_write(hid_keyboard_dev, keyboard_report, sizeof(keyboard_report), NULL);
 }
 
 void main(void)
 {
-	uint8_t report[4] = { 0x00 };
-	const struct device *hid_dev;
+	struct hid_ops hidops_mouse = {
+		.int_in_ready = int_in_ready_mouse,
+	};
 
-	hid_dev = device_get_binding("HID_0");
+	struct hid_ops hidops_keyboard = {
+		.int_in_ready = int_in_ready_keyboard,
+	};
+
+	hid_keyboard_dev = device_get_binding("HID_0");
+	hid_mouse_dev = device_get_binding("HID_1");
+
 	gpio_pin_configure_dt(&led0, GPIO_OUTPUT);
+	gpio_pin_configure_dt(&sw0, GPIO_INPUT);
+	gpio_pin_configure_dt(&sw1, GPIO_INPUT);
+	gpio_pin_configure_dt(&sw2, GPIO_INPUT);
+	gpio_pin_configure_dt(&sw3, GPIO_INPUT);
 
-	callbacks_configure(&sw0, &left_button, &callback[0], &def_val[0]);
-	callbacks_configure(&sw1, &right_button, &callback[1], &def_val[1]);
-	callbacks_configure(&sw2, &x_move, &callback[2], &def_val[2]);
-	callbacks_configure(&sw3, &y_move, &callback[3], &def_val[3]);
+	usb_hid_register_device(hid_keyboard_dev, hid_keyboard_report_desc, sizeof(hid_keyboard_report_desc), &hidops_keyboard);
+	usb_hid_register_device(hid_mouse_dev, hid_mouse_report_desc, sizeof(hid_mouse_report_desc), &hidops_mouse);
 
-	usb_hid_register_device(hid_dev, hid_report_desc, sizeof(hid_report_desc), NULL);
-	usb_hid_init(hid_dev);
+	usb_hid_init(hid_keyboard_dev);
+	usb_hid_init(hid_mouse_dev);
+
 	usb_enable(status_cb);
 
 	while (true) {
-		k_sem_take(&sem, K_FOREVER);
-
-		report[MOUSE_BTN_REPORT_POS] = status[MOUSE_BTN_REPORT_POS];
-		report[MOUSE_X_REPORT_POS] = status[MOUSE_X_REPORT_POS];
-		status[MOUSE_X_REPORT_POS] = 0U;
-		report[MOUSE_Y_REPORT_POS] = status[MOUSE_Y_REPORT_POS];
-		status[MOUSE_Y_REPORT_POS] = 0U;
-
-		hid_int_ep_write(hid_dev, report, sizeof(report), NULL);
-		gpio_pin_toggle(led0.port, led0.pin);
 	}
 }
