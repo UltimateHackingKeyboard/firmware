a3cd8b329c949892dc33980c4e335950758e7a8b
László Monda <laci@monda.hu>
2022-11-06 17:10:33 +0100

    Simply write "a" characters via Bluetooth keyboard HID instad of the "hello\n" string.

--- nordic-left/src/main.c
+++ nordic-left/src/main.c
@@ -179,15 +179,6 @@ static struct conn_mode {
 	bool in_boot_mode;
 } conn_mode;
 
-static const uint8_t hello_world_str[] = {
-	0x0b,	/* Key h */
-	0x08,	/* Key e */
-	0x0f,	/* Key l */
-	0x0f,	/* Key l */
-	0x12,	/* Key o */
-	0x28,	/* Key Return */
-};
-
 /* Current report status
  */
 static struct keyboard_state {
@@ -536,30 +527,12 @@ static struct bt_conn_auth_info_cb conn_auth_info_callbacks = {
 	.pairing_failed = pairing_failed
 };
 
-/** @brief Function process keyboard state and sends it
- *
- *  @param pstate     The state to be sent
- *  @param boot_mode  Information if boot mode protocol is selected.
- *  @param conn       Connection handler
- *
- *  @return 0 on success or negative error code.
- */
-static int key_report_con_send(const struct keyboard_state *state, bool boot_mode, struct bt_conn *conn)
+static int key_report_con_send(const struct keyboard_state *state, bool boot_mode, struct bt_conn *conn, bool down)
 {
+	uint8_t chr = down ? HID_KEY_A : 0;
+	uint8_t  data[INPUT_REPORT_KEYS_MAX_LEN] = {state->ctrl_keys_state, 0, chr, 0, 0, 0, 0, 0};
+
 	int err = 0;
-	uint8_t  data[INPUT_REPORT_KEYS_MAX_LEN];
-	uint8_t *key_data;
-	const uint8_t *key_state;
-	size_t n;
-
-	data[0] = state->ctrl_keys_state;
-	data[1] = 0;
-	key_data = &data[2];
-	key_state = state->keys_state;
-
-	for (n = 0; n < KEY_PRESS_MAX; ++n) {
-		*key_data++ = *key_state++;
-	}
 	if (boot_mode) {
 		err = bt_hids_boot_kb_inp_rep_send(&hids_obj, conn, data, sizeof(data), NULL);
 	} else {
@@ -568,103 +541,16 @@ static int key_report_con_send(const struct keyboard_state *state, bool boot_mod
 	return err;
 }
 
-/** @brief Function process and send keyboard state to all active connections
- *
- * Function process global keyboard state and send it to all connected
- * clients.
- *
- * @return 0 on success or negative error code.
- */
-static int key_report_send(void)
+static int key_report_send(bool down)
 {
 	if (conn_mode.conn) {
-		int err = key_report_con_send(&hid_keyboard_state, conn_mode.in_boot_mode, conn_mode.conn);
+		int err = key_report_con_send(&hid_keyboard_state, conn_mode.in_boot_mode, conn_mode.conn, down);
 		if (err) {
 			printk("Key report send error: %d\n", err);
 			return err;
 		}
 	}
 	return 0;
-}
-
-static int hid_kbd_state_key_set(uint8_t key)
-{
-	for (size_t i = 0; i < KEY_CTRL_CODE_MAX; ++i) {
-		if (hid_keyboard_state.keys_state[i] == 0) {
-			hid_keyboard_state.keys_state[i] = key;
-			return 0;
-		}
-	}
-	/* All slots busy */
-	return -EBUSY;
-}
-
-static int hid_kbd_state_key_clear(uint8_t key)
-{
-	for (size_t i = 0; i < KEY_CTRL_CODE_MAX; ++i) {
-		if (hid_keyboard_state.keys_state[i] == key) {
-			hid_keyboard_state.keys_state[i] = 0;
-			return 0;
-		}
-	}
-	/* Key not found */
-	return -EINVAL;
-}
-
-/** @brief Press a button and send report
- *
- *  @note Functions to manipulate hid state are not reentrant
- *  @param keys
- *  @param cnt
- *
- *  @return 0 on success or negative error code.
- */
-static int hid_buttons_press(const uint8_t *keys, size_t cnt)
-{
-	while (cnt--) {
-		int err = hid_kbd_state_key_set(*keys++);
-		if (err) {
-			printk("Cannot set selected key.\n");
-			return err;
-		}
-	}
-
-	return key_report_send();
-}
-
-/** @brief Release the button and send report
- *
- *  @note Functions to manipulate hid state are not reentrant
- *  @param keys
- *  @param cnt
- *
- *  @return 0 on success or negative error code.
- */
-static int hid_buttons_release(const uint8_t *keys, size_t cnt)
-{
-	while (cnt--) {
-		int err;
-		err = hid_kbd_state_key_clear(*keys++);
-		if (err) {
-			printk("Cannot clear selected key.\n");
-			return err;
-		}
-	}
-
-	return key_report_send();
-}
-
-static void button_text_changed(bool down)
-{
-	static const uint8_t *chr = hello_world_str;
-	if (down) {
-		hid_buttons_press(chr, 1);
-	} else {
-		hid_buttons_release(chr, 1);
-		if (++chr == (hello_world_str + sizeof(hello_world_str))) {
-			chr = hello_world_str;
-		}
-	}
 }
 
 static void num_comp_reply(bool accept)
@@ -721,7 +607,7 @@ static void button_changed(uint32_t button_state, uint32_t has_changed)
 	}
 
 	if (has_changed & KEY_TEXT_MASK) {
-		button_text_changed((button_state & KEY_TEXT_MASK) != 0);
+		key_report_send((button_state & KEY_TEXT_MASK) != 0);
 	}
 }
 
