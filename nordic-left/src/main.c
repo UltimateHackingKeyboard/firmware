0c1826dc77fab8bee4c6871cce09dc0f1a36e8be
László Monda <laci@monda.hu>
2022-11-22 23:56:39 +0100

    Make the left half receive characters from the right half over UART.

--- nordic-left/src/main.c
+++ nordic-left/src/main.c
@@ -1,6 +1,7 @@
 #include <zephyr/zephyr.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/uart.h>
 
 #include <zephyr/types.h>
 #include <stddef.h>
@@ -17,13 +18,38 @@
 #include "usb.h"
 #include "bluetooth.h"
 
+static const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
+
+void serial_cb(const struct device *dev, void *user_data)
+{
+	if (!uart_irq_update(uart_dev)) {
+		return;
+	}
+
+	uint8_t c;
+	while (uart_irq_rx_ready(uart_dev)) {
+		uart_fifo_read(uart_dev, &c, 1);
+		printk("uart1 receive: %c\n", c);
+	}
+}
+
 void main(void) {
+	printk("UHK 80 nordic-left");
+
+	if (!device_is_ready(uart_dev)) {
+		printk("UART device not found!");
+		return;
+	}
+
+
 	dk_buttons_init(button_changed);
 	dk_leds_init();
 
 	usb_init();
 	bluetooth_init();
 
+	uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL);
+	uart_irq_rx_enable(uart_dev);
 	int blink_status = 0;
 	for (;;) {
 		bluetooth_set_adv_led(&blink_status);
