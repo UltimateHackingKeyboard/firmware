cd774280439e5b024b9df7f9feb1115809b1172b
Karel Tuček <kareltucek@centrum.cz>
2022-12-06 14:45:18 +0100

    Fix mistyped less-or-equal operator.

--- right/src/caret_config.c
+++ right/src/caret_config.c
@@ -60,10 +60,10 @@ caret_configuration_t remappableModes[] = {
 };
 
 caret_configuration_t* GetNavigationModeConfiguration(navigation_mode_t mode) {
-    if (NavigationMode_RemappableFirst <= mode && mode < NavigationMode_RemappableLast) {
+    if (NavigationMode_RemappableFirst <= mode && mode <= NavigationMode_RemappableLast) {
         return &remappableModes[mode - NavigationMode_RemappableFirst];
     } else {
-            Macros_ReportErrorNum("Mode referenced in in valid context:", mode);
+            Macros_ReportErrorNum("Mode referenced in invalid context. Only remappable modes are supported here.", mode);
             return NULL;
     }
 }
