ec78de5f4585f48f1eae24a28f81e067f4e905ca
László Monda <laci@monda.hu>
2022-12-30 22:27:46 +0100

    Implement writeSpi()

--- nordic-right/src/main.c
+++ nordic-right/src/main.c
@@ -6,30 +6,39 @@
 
 #include <drivers/spi.h>
 
-static struct spi_config spi_conf = {
+static struct spi_config spiConf = {
 	.frequency = 400000U,
 	.operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB)
 };
 
+uint8_t buf[] = {1};
+const struct spi_buf spiBuf[] = {
+	{
+		.buf = &buf,
+		.len = 1,
+	}
+};
+
+const struct spi_buf_set spiBufSet = {
+	.buffers = spiBuf,
+	.count = 1,
+};
+
+const struct device *spi0_dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
+
+void writeSpi(uint8_t data)
+{
+	buf[0] = data;
+	spi_write(spi0_dev, &spiConf, &spiBufSet);
+}
+
 void main(void)
 {
-	uint8_t buf[] = {1, 2, 3};
-    const struct spi_buf spiBuf[] = {
-		{
-			.buf = &buf,
-			.len = 3,
-    	}
-	};
-
-    const struct spi_buf_set spiBufSet = {
-        .buffers = spiBuf,
-        .count = 1,
-	};
-	const struct device *spi0_dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
-
 	while (true) {
-		printk("spi send: a\n");
-		spi_write(spi0_dev, &spi_conf, &spiBufSet);
-//        k_msleep(1);
+//		printk("spi send: a\n");
+		for (uint8_t i=0; i<4; i++) {
+			writeSpi(i);
+		}
+        k_msleep(1);
 	}
 }
