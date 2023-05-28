7098ca8c3a34caba84b4dbce817fa081a56ca6a6
László Monda <laci@monda.hu>
2023-05-28 23:41:09 +0200

    Uncomment OLED and LED code.

--- nordic-right/src/main.c
+++ nordic-right/src/main.c
@@ -8,50 +8,50 @@
 
 #define LedPagePrefix 0b01010000
 
-// static struct spi_config spiConf = {
-//     .frequency = 400000U,
-//     .operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB)
-// };
+static struct spi_config spiConf = {
+    .frequency = 400000U,
+    .operation = (SPI_OP_MODE_MASTER | SPI_WORD_SET(8) | SPI_TRANSFER_MSB)
+};
 
-// uint8_t buf[] = {1};
-// const struct spi_buf spiBuf[] = {
-//     {
-//         .buf = &buf,
-//         .len = 1,
-//     }
-// };
+uint8_t buf[] = {1};
+const struct spi_buf spiBuf[] = {
+    {
+        .buf = &buf,
+        .len = 1,
+    }
+};
 
-// const struct spi_buf_set spiBufSet = {
-//     .buffers = spiBuf,
-//     .count = 1,
-// };
+const struct spi_buf_set spiBufSet = {
+    .buffers = spiBuf,
+    .count = 1,
+};
 
-// const struct device *spi0_dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
+const struct device *spi0_dev = DEVICE_DT_GET(DT_NODELABEL(spi1));
 static const struct gpio_dt_spec oledCsEn = GPIO_DT_SPEC_GET(DT_ALIAS(oled_en), gpios);
-// static const struct gpio_dt_spec oledCsDt = GPIO_DT_SPEC_GET(DT_ALIAS(oled_cs), gpios);
-// static const struct gpio_dt_spec ledsCsDt = GPIO_DT_SPEC_GET(DT_ALIAS(leds_cs), gpios);
-// static const struct gpio_dt_spec oledA0Dt = GPIO_DT_SPEC_GET(DT_ALIAS(oled_a0), gpios);
+static const struct gpio_dt_spec oledCsDt = GPIO_DT_SPEC_GET(DT_ALIAS(oled_cs), gpios);
+static const struct gpio_dt_spec ledsCsDt = GPIO_DT_SPEC_GET(DT_ALIAS(leds_cs), gpios);
+static const struct gpio_dt_spec oledA0Dt = GPIO_DT_SPEC_GET(DT_ALIAS(oled_a0), gpios);
 
-// void setLedsCs(bool state)
-// {
-//     gpio_pin_set_dt(&ledsCsDt, state);
-// }
+void setLedsCs(bool state)
+{
+    gpio_pin_set_dt(&ledsCsDt, state);
+}
 
-// void setOledCs(bool state)
-// {
-//     gpio_pin_set_dt(&oledCsDt, state);
-// }
+void setOledCs(bool state)
+{
+    gpio_pin_set_dt(&oledCsDt, state);
+}
 
-// void setA0(bool state)
-// {
-//     gpio_pin_set_dt(&oledA0Dt, state);
-// }
+void setA0(bool state)
+{
+    gpio_pin_set_dt(&oledA0Dt, state);
+}
 
-// void writeSpi(uint8_t data)
-// {
-//     buf[0] = data;
-//     spi_write(spi0_dev, &spiConf, &spiBufSet);
-// }
+void writeSpi(uint8_t data)
+{
+    buf[0] = data;
+    spi_write(spi0_dev, &spiConf, &spiBufSet);
+}
 
 // static const struct gpio_dt_spec row1 = GPIO_DT_SPEC_GET(DT_ALIAS(row1), gpios);
 // static const struct gpio_dt_spec row2 = GPIO_DT_SPEC_GET(DT_ALIAS(row2), gpios);
@@ -94,18 +94,12 @@ static const struct gpio_dt_spec oledCsEn = GPIO_DT_SPEC_GET(DT_ALIAS(oled_en), 
 
 void main(void)
 {
-    gpio_pin_configure_dt(&oledCsEn, GPIO_OUTPUT);
-    bool state = true;
-    while (true) {
-        gpio_pin_set_dt(&oledCsEn, state);
-//        state = !state;
-        k_msleep(1000);
-    }
+    gpio_pin_configure_dt(&oledCsEn, GPIO_OUTPUT);    gpio_pin_set_dt(&oledCsEn, true);
 
 //  struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
-//     gpio_pin_configure_dt(&oledCsDt, GPIO_OUTPUT);
-//     gpio_pin_configure_dt(&ledsCsDt, GPIO_OUTPUT);
-//     gpio_pin_configure_dt(&oledA0Dt, GPIO_OUTPUT);
+    gpio_pin_configure_dt(&oledCsDt, GPIO_OUTPUT);
+    gpio_pin_configure_dt(&ledsCsDt, GPIO_OUTPUT);
+    gpio_pin_configure_dt(&oledA0Dt, GPIO_OUTPUT);
 
 //     for (uint8_t rowId=0; rowId<6; rowId++) {
 //         gpio_pin_configure_dt(&rows[rowId], GPIO_OUTPUT);
@@ -114,9 +108,9 @@ void main(void)
 //         gpio_pin_configure_dt(&cols[colId], GPIO_INPUT);
 //     }
 
-//     uint32_t counter = 0;
-//     bool pixel = 1;
-//     while (true) {
+    uint32_t counter = 0;
+    bool pixel = 1;
+    while (true) {
 //         printk(".");
 //         for (uint8_t rowId=0; rowId<6; rowId++) {
 //             gpio_pin_set_dt(&rows[rowId], 1);
@@ -130,56 +124,56 @@ void main(void)
 
 // //      uart_poll_out(uart_dev, 'a');
 
-// //      printk("spi send: a\n");
-//         setA0(false);
-//         setOledCs(false);
-//         writeSpi(0xaf);
-//         setOledCs(true);
+//      printk("spi send: a\n");
+        setA0(false);
+        setOledCs(false);
+        writeSpi(0xaf);
+        setOledCs(true);
 
-//         setA0(false);
-//         setOledCs(false);
-//         writeSpi(0x81);
-//         writeSpi(0xff);
-//         setOledCs(true);
+        setA0(false);
+        setOledCs(false);
+        writeSpi(0x81);
+        writeSpi(0xff);
+        setOledCs(true);
 
-//         setA0(true);
-//         setOledCs(false);
-//         writeSpi(pixel ? 0xff : 0x00);
-//         setOledCs(true);
+        setA0(true);
+        setOledCs(false);
+        writeSpi(pixel ? 0xff : 0x00);
+        setOledCs(true);
 
-//         setLedsCs(false);
-//         writeSpi(LedPagePrefix | 2);
-//         writeSpi(0x00);
-//         writeSpi(0b00001001);
-//         setLedsCs(true);
+        setLedsCs(false);
+        writeSpi(LedPagePrefix | 2);
+        writeSpi(0x00);
+        writeSpi(0b00001001);
+        setLedsCs(true);
 
-//         setLedsCs(false);
-//         writeSpi(LedPagePrefix | 2);
-//         writeSpi(0x01);
-//         writeSpi(0xff);
-//         setLedsCs(true);
+        setLedsCs(false);
+        writeSpi(LedPagePrefix | 2);
+        writeSpi(0x01);
+        writeSpi(0xff);
+        setLedsCs(true);
 
-//         setLedsCs(false);
-//         writeSpi(LedPagePrefix | 0);
-//         writeSpi(0x00);
-//         for (int i=0; i<255; i++) {
-//             writeSpi(0xff);
-//         }
-//         setLedsCs(true);
+        setLedsCs(false);
+        writeSpi(LedPagePrefix | 0);
+        writeSpi(0x00);
+        for (int i=0; i<255; i++) {
+            writeSpi(0xff);
+        }
+        setLedsCs(true);
 
-//         setLedsCs(false);
-//         writeSpi(LedPagePrefix | 1);
-//         writeSpi(0x00);
-//         for (int i=0; i<255; i++) {
-//             writeSpi(0xff);
-//         }
-//         setLedsCs(true);
+        setLedsCs(false);
+        writeSpi(LedPagePrefix | 1);
+        writeSpi(0x00);
+        for (int i=0; i<255; i++) {
+            writeSpi(0xff);
+        }
+        setLedsCs(true);
 
-// //        k_msleep(1);
+//        k_msleep(1);
 
-//         if (counter++ > 19) {
-//             pixel = !pixel;
-//             counter = 0;
-//         }
-//     }
+        if (counter++ > 19) {
+            pixel = !pixel;
+            counter = 0;
+        }
+    }
 }
