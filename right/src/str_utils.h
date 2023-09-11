8931742b5263d16658d4a73dbe9990e643bc123f
Karel Tuček <kareltucek@centrum.cz>
2023-09-12 00:36:07 +0200

    Fix comments in string literals.

--- right/src/str_utils.h
+++ right/src/str_utils.h
@@ -33,6 +33,7 @@
     bool StrEqual(const char* a, const char* aEnd, const char* b, const char* bEnd);
     const char* FindChar(char c, const char* str, const char* strEnd);
     bool ConsumeToken(parser_context_t* ctx, const char *b);
+    void ConsumeCommentsAsWhite(bool consume);
     bool ConsumeTokenByRef(parser_context_t* ctx, string_ref_t ref);
     bool ConsumeIdentifierByRef(parser_context_t* ctx, string_ref_t ref);
     void ConsumeAnyIdentifier(parser_context_t* ctx);
