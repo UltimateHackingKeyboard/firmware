443283a97fcd909a80521394657f9bd24c37661f
László Monda <laci@monda.hu>
2023-09-22 17:17:04 +0200

    Convert tabs to spaces.

--- nordic-left/src/main.cpp
+++ nordic-left/src/main.cpp
@@ -26,10 +26,10 @@ extern "C"
 #include <zephyr/drivers/adc.h>
 
 #define DT_SPEC_AND_COMMA(node_id, prop, idx) \
-	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+    ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
 
 static const struct adc_dt_spec adc_channels[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, DT_SPEC_AND_COMMA)
+    DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels, DT_SPEC_AND_COMMA)
 };
 
 #define LedPagePrefix 0b01010000
@@ -150,25 +150,25 @@ int main(void) {
     // Init ADC channels
 
     int err;
-	uint32_t count = 0;
-	uint16_t buf;
-	struct adc_sequence sequence = {
-		.buffer = &buf,
-		.buffer_size = sizeof(buf),
-	};
+    uint32_t count = 0;
+    uint16_t buf;
+    struct adc_sequence sequence = {
+        .buffer = &buf,
+        .buffer_size = sizeof(buf),
+    };
 
-	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		if (!device_is_ready(adc_channels[i].dev)) {
-			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
-			return 0;
-		}
+    for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+        if (!device_is_ready(adc_channels[i].dev)) {
+            printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+            return 0;
+        }
 
-		err = adc_channel_setup_dt(&adc_channels[i]);
-		if (err < 0) {
-			printk("Could not setup channel #%d (%d)\n", i, err);
-			return 0;
-		}
-	}
+        err = adc_channel_setup_dt(&adc_channels[i]);
+        if (err < 0) {
+            printk("Could not setup channel #%d (%d)\n", i, err);
+            return 0;
+        }
+    }
 
     // if (!device_is_ready(uart_dev)) {
     //     printk("UART device not found!");
