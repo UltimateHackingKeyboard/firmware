3c0cd410dcb9634e5dd0d607065cd57ac57931dd
László Monda <laci@monda.hu>
2023-11-06 00:18:47 +0100

    Make the structure of device.h reflect the structure of other header files.

--- master/src/device.h
+++ master/src/device.h
@@ -1,33 +1,42 @@
-#include "autoconf.h"
+#ifndef __DEVICE_H__
+#define __DEVICE_H__
 
-#define DEVICE_ID_UHK60V1_RIGHT 1
-#define DEVICE_ID_UHK60V2_RIGHT 2
-#define DEVICE_ID_UHK80_LEFT 3
-#define DEVICE_ID_UHK80_RIGHT 4
+// Includes:
 
-#if CONFIG_DEVICE_ID == DEVICE_ID_UHK60V1_RIGHT
-    #define DEVICE_NAME "UHK 60 v1"
-    #define USB_DEVICE_PRODUCT_ID 0x6122
-#elif CONFIG_DEVICE_ID == DEVICE_ID_UHK60V2_RIGHT
-    #define DEVICE_NAME "UHK 60 v2"
-    #define USB_DEVICE_PRODUCT_ID 0x6124
-#elif CONFIG_DEVICE_ID == DEVICE_ID_UHK80_LEFT
-    #define DEVICE_NAME "UHK 80 left half"
-    #define USB_DEVICE_PRODUCT_ID 0xffff // TODO
-#elif CONFIG_DEVICE_ID == DEVICE_ID_UHK80_RIGHT
-    #define DEVICE_NAME "UHK 80 right half"
-    #define USB_DEVICE_PRODUCT_ID 0xffff // TODO
-#endif
+    #include "autoconf.h"
 
-#if CONFIG_DEVICE_ID == DEVICE_ID_UHK80_LEFT
-    #define DEVICE_HAS_MERGE_SENSE
-#endif
+// Macros:
 
-#if CONFIG_DEVICE_ID == DEVICE_ID_UHK80_RIGHT
-    #define DEVICE_HAS_OLED
-#endif
+    #define DEVICE_ID_UHK60V1_RIGHT 1
+    #define DEVICE_ID_UHK60V2_RIGHT 2
+    #define DEVICE_ID_UHK80_LEFT 3
+    #define DEVICE_ID_UHK80_RIGHT 4
 
-#if CONFIG_DEVICE_ID == DEVICE_ID_UHK80_LEFT || CONFIG_DEVICE_ID == DEVICE_ID_UHK80_RIGHT
-    #define DEVICE_HAS_NRF
-    #define DEVICE_HAS_BATTERY
+    #if CONFIG_DEVICE_ID == DEVICE_ID_UHK60V1_RIGHT
+        #define DEVICE_NAME "UHK 60 v1"
+        #define USB_DEVICE_PRODUCT_ID 0x6122
+    #elif CONFIG_DEVICE_ID == DEVICE_ID_UHK60V2_RIGHT
+        #define DEVICE_NAME "UHK 60 v2"
+        #define USB_DEVICE_PRODUCT_ID 0x6124
+    #elif CONFIG_DEVICE_ID == DEVICE_ID_UHK80_LEFT
+        #define DEVICE_NAME "UHK 80 left half"
+        #define USB_DEVICE_PRODUCT_ID 0xffff // TODO
+    #elif CONFIG_DEVICE_ID == DEVICE_ID_UHK80_RIGHT
+        #define DEVICE_NAME "UHK 80 right half"
+        #define USB_DEVICE_PRODUCT_ID 0xffff // TODO
+    #endif
+
+    #if CONFIG_DEVICE_ID == DEVICE_ID_UHK80_LEFT
+        #define DEVICE_HAS_MERGE_SENSE
+    #endif
+
+    #if CONFIG_DEVICE_ID == DEVICE_ID_UHK80_RIGHT
+        #define DEVICE_HAS_OLED
+    #endif
+
+    #if CONFIG_DEVICE_ID == DEVICE_ID_UHK80_LEFT || CONFIG_DEVICE_ID == DEVICE_ID_UHK80_RIGHT
+        #define DEVICE_HAS_NRF
+        #define DEVICE_HAS_BATTERY
+    #endif
+
 #endif
