d4a0ccda81f1bb7b6e977ffb9f554de386305396
László Monda <laci@monda.hu>
2023-11-10 22:12:02 +0100

    Don't expose mergeSenseDt

--- master/src/merge_sensor.h
+++ master/src/merge_sensor.h
@@ -1,10 +1,6 @@
 #ifndef __MERGE_SENSOR_H__
 #define __MERGE_SENSOR_H__
 
-// Variables:
-
-    extern const struct gpio_dt_spec mergeSenseDt;
-
 // Functions:
 
     extern void InitMergeSensor(void);
