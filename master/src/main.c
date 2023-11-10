7ba01a398d3ee59508d1787af673d51776a58b6b
László Monda <laci@monda.hu>
2023-11-10 21:50:42 +0100

    Add commented out code to blink the test LED for the testing of the UHK 60 build.

--- master/src/main.c
+++ master/src/main.c
@@ -10,9 +10,18 @@
 #include "shell.h"
 #include "device.h"
 #include "usb/usb.hpp"
+#include <zephyr/drivers/gpio.h>
+
+const struct gpio_dt_spec testLed = GPIO_DT_SPEC_GET(DT_ALIAS(test_led), gpios);
 
 int main(void) {
     printk("----------\n" DEVICE_NAME " started\n");
+
+    // while (true) {
+    //     gpio_pin_configure_dt(&testLed, GPIO_OUTPUT);
+    //     gpio_pin_toggle_dt(&testLed);
+    //     k_msleep(1000);
+    // }
 
     InitUart();
     InitI2c();
