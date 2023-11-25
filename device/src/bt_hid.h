11f0e929c3f8451be61b860682ece2b69c61cf62
László Monda <laci@monda.hu>
2023-11-25 17:12:05 +0100

    Structure bt_hid.h just like the rest of the headers.

--- device/src/bt_hid.h
+++ device/src/bt_hid.h
@@ -1,6 +1,15 @@
-#include <zephyr/types.h>
+#ifndef __BT_HID_H__
+#define __BT_HID_H__
 
-extern void bas_notify(void);
-extern void bluetooth_init(void);
-extern void num_comp_reply(uint8_t accept);
-extern void key_report_send(uint8_t down);
+// Includes:
+
+    #include <zephyr/types.h>
+
+// Functions:
+
+    extern void bas_notify(void);
+    extern void bluetooth_init(void);
+    extern void num_comp_reply(uint8_t accept);
+    extern void key_report_send(uint8_t down);
+
+#endif // __BT_HID_H__
