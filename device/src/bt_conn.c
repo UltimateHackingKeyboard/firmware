395e169d047c68481f65a5d81510c35270c12544
László Monda <laci@monda.hu>
2023-11-30 21:09:16 +0100

    Add peer addresses.

--- device/src/bt_conn.c
+++ device/src/bt_conn.c
@@ -1,6 +1,10 @@
 #include "bt_advertise.h"
 #include "bt_hid.h"
 
+bt_addr_le_t leftAddr = { .a={ 0xe7, 0xf5, 0x5d, 0x7c, 0x82, 0x35 }, .type=BT_ADDR_LE_RANDOM };
+bt_addr_le_t rightAddr = { .a={ 0xf0, 0x98, 0x11, 0xce, 0xa4, 0x92 }, .type=BT_ADDR_LE_RANDOM };
+bt_addr_le_t dongleAddr = { .a={ 0x01, 0x23, 0x45, 0x67, 0x89, 0xab }, .type=BT_ADDR_LE_RANDOM };
+
 static void connected(struct bt_conn *conn, uint8_t err) {
     char addr[BT_ADDR_LE_STR_LEN];
     bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
