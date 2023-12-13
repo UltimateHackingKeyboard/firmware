df749cd5dce6f603d59f3c3a2d20a45d7f6c0c56
László Monda <laci@monda.hu>
2023-12-13 08:55:38 +0100

    Delete redundant includes.

--- device/src/bt_scan.c
+++ device/src/bt_scan.c
@@ -1,26 +1,4 @@
-#include <errno.h>
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/sys/byteorder.h>
-#include <zephyr/sys/printk.h>
-
-#include <zephyr/bluetooth/bluetooth.h>
-#include <zephyr/bluetooth/hci.h>
-#include <zephyr/bluetooth/conn.h>
-#include <zephyr/bluetooth/uuid.h>
-#include <zephyr/bluetooth/gatt.h>
-
-#include <bluetooth/services/nus.h>
-#include <bluetooth/services/nus_client.h>
-#include <bluetooth/gatt_dm.h>
 #include <bluetooth/scan.h>
-
-#include <zephyr/settings/settings.h>
-
-#include <zephyr/drivers/uart.h>
-
-#include <zephyr/logging/log.h>
 
 struct bt_conn *bt_uart_conn;
 
