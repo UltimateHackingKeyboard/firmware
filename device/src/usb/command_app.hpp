9d4af936d0f2ce78edffb2ddd2d5e8111fd767fb
Benedek Kupper <kupper.benedek@gmail.com>
2023-12-17 23:16:27 +0100

    add command protocol C interface for sending and receiving:
    define void CommandProtocolRxHandler(const uint8_t* data, size_t size)
    and call bool CommandProtocolTx(const uint8_t* data, size_t size)

--- device/src/usb/command_app.hpp
+++ device/src/usb/command_app.hpp
@@ -1,6 +1,7 @@
 #ifndef __COMMAND_APP_HEADER__
 #define __COMMAND_APP_HEADER__
 
+#include "double_buffer.hpp"
 #include "hid/application.hpp"
 
 class command_app : public hid::application
@@ -11,29 +12,30 @@ class command_app : public hid::application
         : application(report_protocol())
     {}
 
-public:
+  public:
     static constexpr size_t MESSAGE_SIZE = 64;
     static constexpr uint8_t REPORT_ID = 0;
-    
-    template<hid::report::type TYPE>
+
+    template <hid::report::type TYPE>
     struct report_base : public hid::report::base<TYPE, 0>
     {
-        std::array<uint8_t, MESSAGE_SIZE> payload {};
+        std::array<uint8_t, MESSAGE_SIZE> payload{};
     };
     using report_in = report_base<hid::report::type::INPUT>;
     using report_out = report_base<hid::report::type::OUTPUT>;
 
     static command_app& handle();
 
-    using hid::application::send_report;
+    bool send(std::span<const uint8_t> buffer);
 
     void start(hid::protocol prot) override;
     void set_report(hid::report::type type, const std::span<const uint8_t>& data) override;
     void get_report(hid::report::selector select, const std::span<uint8_t>& buffer) override;
+    void in_report_sent(const std::span<const uint8_t>& data) override;
 
-private:
-    report_in in_buffer_{};
-    report_out out_buffer_{};
+  private:
+    double_buffer<report_in> in_buffer_{};
+    double_buffer<report_out> out_buffer_{};
 };
 
 #endif // __COMMAND_APP_HEADER__
