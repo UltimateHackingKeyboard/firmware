a4f792b8076db0de017af4c6a503011688ef89c6
László Monda <laci@monda.hu>
2023-12-29 00:31:04 +0100

    Delete UHK 60 specific LED test code.

--- device/src/main.c
+++ device/src/main.c
@@ -16,16 +16,8 @@
 #include "bt_conn.h"
 #include "settings.h"
 
-//const struct gpio_dt_spec testLed = GPIO_DT_SPEC_GET(DT_ALIAS(test_led), gpios);
-
 int main(void) {
     printk("----------\n" DEVICE_NAME " started\n");
-
-    // while (true) {
-    //     gpio_pin_configure_dt(&testLed, GPIO_OUTPUT);
-    //     gpio_pin_toggle_dt(&testLed);
-    //     k_msleep(1000);
-    // }
 
     InitUart();
     InitI2c();
