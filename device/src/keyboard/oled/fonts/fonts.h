a6c421772547fa8765b58cb27a324b3f9d8ccb81
Karel Tuček <kareltucek@centrum.cz>
2024-02-18 12:58:43 +0100

    Remove (dead) terminus links.

--- device/src/keyboard/oled/fonts/fonts.h
+++ device/src/keyboard/oled/fonts/fonts.h
@@ -12,10 +12,5 @@ extern const lv_font_t JetBrainsMono12;
 extern const lv_font_t JetBrainsMono16;
 extern const lv_font_t JetBrainsMono24;
 extern const lv_font_t JetBrainsMono32;
-extern const lv_font_t Terminus5;
-extern const lv_font_t Terminus6;
-extern const lv_font_t Terminus7;
-extern const lv_font_t Terminus8;
-extern const lv_font_t Terminus10;
 
 #endif
