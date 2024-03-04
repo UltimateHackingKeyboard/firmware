ef6500e2c0de2921573c9d8e1be4c1665f573b26
Benedek Kupper <kupper.benedek@gmail.com>
2024-03-04 22:46:55 +0100

    bugfix for C build

--- device/src/keyboard/oled/widgets/widget.c
+++ device/src/keyboard/oled/widgets/widget.c
@@ -1,11 +1,14 @@
 #include "widget.h"
 #include "../oled.h"
+#include "device.h"
 #include <stdlib.h>
 
 void Widget_RequestRedraw(widget_t* widget)
 {
+#ifdef DEVICE_HAS_OLED
     if (widget != NULL) {
         widget->dirty = true;
     }
     Oled_RequestRedraw();
+#endif
 }
