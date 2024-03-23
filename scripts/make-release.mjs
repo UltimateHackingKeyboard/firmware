48ad8ca579209ba3af15d420670946f8a0f9a071
László Monda <laci@monda.hu>
2024-03-23 22:18:28 +0100

    Convert scripts to .mjs

--- scripts/make-release.mjs
+++ scripts/make-release.mjs
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const common = require('./common.js');
-require('shelljs/global');
+import fs from 'fs';
+import path from 'path';
+import {getGitInfo} from './common.mjs';
+import shell from 'shelljs';
+import {generateVersionsH} from './generate-versions-h-util.mjs';
+import {readPackageJson} from './read-package-json.mjs';
+import { fileURLToPath } from 'url';
 
-const generateVersionsH = require('./generate-versions-h-util.js');
-const readPackageJson = require('./read-package-json.js');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-config.fatal = true;
-config.verbose = true;
+shell.config.fatal = true;
+shell.config.verbose = true;
 
-const gitInfo = common.getGitInfo();
+const gitInfo = getGitInfo();
 const packageJson = readPackageJson();
 
 generateVersionsH({ packageJson, gitInfo, useRealData: false });
@@ -33,7 +36,7 @@ if (gitInfo.tag !== `v${version}`) {
 
 const deviceSourceFirmwares = packageJson.devices.map(device => `${__dirname}/../${device.source}`);
 const moduleSourceFirmwares = packageJson.modules.map(module => `${__dirname}/../${module.source}`);
-rm('-rf', releaseDir, releaseFile, deviceSourceFirmwares, moduleSourceFirmwares);
+shell.rm('-rf', releaseDir, releaseFile, deviceSourceFirmwares, moduleSourceFirmwares);
 
 const sourcePaths = [
     ...packageJson.devices.map(device => device.source),
@@ -41,8 +44,8 @@ const sourcePaths = [
 ];
 for (const sourcePath of sourcePaths) {
     const buildDir = path.dirname(`${__dirname}/../${sourcePath}`);
-    mkdir('-p', buildDir);
-    exec(`cd ${buildDir}/..; make clean; make -j8 ${mkArgs}`);
+    shell.mkdir('-p', buildDir);
+    shell.exec(`cd ${buildDir}/..; make clean; make -j8 ${mkArgs}`);
 }
 
 const { devices, modules } = generateVersionsH({ packageJson, gitInfo, useRealData: true });
@@ -51,37 +54,37 @@ packageJson.modules = modules;
 
 for (const sourcePath of sourcePaths) {
     const buildDir = path.dirname(`${__dirname}/../${sourcePath}`);
-    exec(`cd ${buildDir}/..; make -j8 ${mkArgs}`);
+    shell.exec(`cd ${buildDir}/..; make -j8 ${mkArgs}`);
 }
 
 
-exec(`npm ci; npm run build`, { cwd: agentDir });
+shell.exec(`npm ci; npm run build`, { cwd: agentDir });
 
 for (const device of packageJson.devices) {
     const deviceDir = `${releaseDir}/devices/${device.name}`;
     const deviceSource = `${__dirname}/../${device.source}`;
     const deviceMMap = `${__dirname}/../${device.mmap}`;
-    mkdir('-p', deviceDir);
-    chmod(644, deviceSource);
-    cp(deviceSource, `${deviceDir}/firmware.hex`);
-    cp(deviceMMap, `${deviceDir}/firmware.map`);
-    exec(`npm run convert-user-config-to-bin -- ${deviceDir}/config.bin`, { cwd: agentDir });
+    shell.mkdir('-p', deviceDir);
+    shell.chmod(644, deviceSource);
+    shell.cp(deviceSource, `${deviceDir}/firmware.hex`);
+    shell.cp(deviceMMap, `${deviceDir}/firmware.map`);
+    shell.exec(`npm run convert-user-config-to-bin -- ${deviceDir}/config.bin`, { cwd: agentDir });
 }
 
 for (const module of packageJson.modules) {
     const moduleDir = `${releaseDir}/modules`;
     const moduleSource = `${__dirname}/../${module.source}`;
     const moduleMMap = `${__dirname}/../${module.mmap}`;
-    mkdir('-p', moduleDir);
-    chmod(644, moduleSource);
-    cp(moduleSource, `${moduleDir}/${module.name}.bin`);
-    cp(moduleMMap, `${moduleDir}/${module.name}.map`);
+    shell.mkdir('-p', moduleDir);
+    shell.chmod(644, moduleSource);
+    shell.cp(moduleSource, `${moduleDir}/${module.name}.bin`);
+    shell.cp(moduleMMap, `${moduleDir}/${module.name}.map`);
 }
 
 const updatedPackage = Object.assign({}, packageJson, { gitInfo: gitInfo });
 fs.writeFileSync(`${releaseDir}/package.json`, JSON.stringify(updatedPackage, null, 2));
 
-cp('-RL', `${__dirname}/../doc/dist`, `${releaseDir}/doc`);
-mkdir('-p', `${releaseDir}/doc-dev`);
-cp('-RL', `${__dirname}/../doc-dev/reference-manual.md`, `${releaseDir}/doc-dev/reference-manual.md`);
-exec(`tar -czvf ${releaseFile} -C ${releaseDir} .`);
+shell.cp('-RL', `${__dirname}/../doc/dist`, `${releaseDir}/doc`);
+shell.mkdir('-p', `${releaseDir}/doc-dev`);
+shell.cp('-RL', `${__dirname}/../doc-dev/reference-manual.md`, `${releaseDir}/doc-dev/reference-manual.md`);
+shell.exec(`tar -czvf ${releaseFile} -C ${releaseDir} .`);
--- scripts/read-package-json.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-module.exports = function readPackageJson() {
-  return JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'));
-}
