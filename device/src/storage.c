c0704c23dd2dcdf29e4ae0f5b26625f7fa1ed5c5
László Monda <laci@monda.hu>
2024-03-27 13:26:42 +0100

    Add pm_static.yml based on partitions.yml and add user config and hardware config partitions to it.

--- device/src/storage.c
+++ device/src/storage.c
@@ -4,5 +4,5 @@
 uint8_t Storage_LaunchTransfer(eeprom_operation_t operation, config_buffer_id_t config_buffer_id, void (*successCallback))
 {
     const struct flash_area *my_area;
-//    int err = flash_area_open(FIXED_PARTITION_ID(user_config_partition), &my_area);
+    int err = flash_area_open(FIXED_PARTITION_ID(user_config_partition), &my_area);
 }
