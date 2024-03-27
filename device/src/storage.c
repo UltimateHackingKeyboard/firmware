23bc19ea3ee31b7185daa558d16101e7d9d6de3b
László Monda <laci@monda.hu>
2024-03-27 16:57:06 +0100

    Implement Storage_LaunchTransfer()

--- device/src/storage.c
+++ device/src/storage.c
@@ -1,8 +1,19 @@
 #include "storage.h"
 #include "zephyr/storage/flash_map.h"
 
-uint8_t Storage_LaunchTransfer(eeprom_operation_t operation, config_buffer_id_t config_buffer_id, void (*successCallback))
+uint8_t Storage_LaunchTransfer(eeprom_operation_t operation, config_buffer_id_t configBufferId, void (*successCallback))
 {
-    const struct flash_area *my_area;
-    int err = flash_area_open(FIXED_PARTITION_ID(user_config_partition), &my_area);
+    uint8_t configAreaId = configBufferId == ConfigBufferId_HardwareConfig
+        ? FLASH_AREA_ID(hardware_config_partition)
+        : FLASH_AREA_ID(user_config_partition);
+    const struct flash_area *configArea;
+    int err = flash_area_open(configAreaId, &configArea);
+
+    size_t configSize = configBufferId == ConfigBufferId_HardwareConfig ? HARDWARE_CONFIG_SIZE : USER_CONFIG_SIZE;
+    if (operation == EepromOperation_Read) {
+        err = flash_area_read(configArea, 0, ConfigBufferIdToConfigBuffer(configBufferId)->buffer, configSize);
+    } else if (operation == EepromOperation_Write) {
+        err = flash_area_write(configArea, 0, ConfigBufferIdToConfigBuffer(configBufferId)->buffer, configSize);
+    }
+    return err;
 }
