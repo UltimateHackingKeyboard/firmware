da276a512df655ce57608f603ad325b98941bbcf
László Monda <laci@monda.hu>
2024-04-07 23:57:36 +0200

    Make the UART tester not block the main thread by creating its own thread.

--- device/src/keyboard/uart.c
+++ device/src/keyboard/uart.c
@@ -1,6 +1,16 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/uart.h>
 
+// Thread definitions
+
+#define THREAD_STACK_SIZE 1000
+#define THREAD_PRIORITY 5
+
+static K_THREAD_STACK_DEFINE(stack_area, THREAD_STACK_SIZE);
+static struct k_thread thread_data;
+
+// UART definitions
+
 const struct device *uart_dev = DEVICE_DT_GET(DT_NODELABEL(uart1));
 
 #define BUF_SIZE 64
@@ -47,15 +57,25 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
     }
 }
 
-void InitUart(void) {
+void testUart() {
     uint8_t txbuf[5] = {1, 2, 3, 4, 5};
-
-    uart_callback_set(uart_dev, uart_callback, NULL);
-    rxbuf = rxbuf1;
-    uart_rx_enable(uart_dev, rxbuf, BUF_SIZE, 10000);
-
     while (1) {
         uart_tx(uart_dev, txbuf, sizeof(txbuf), 10000);
         k_sleep(K_MSEC(1000));
     }
+}
+
+void InitUart(void) {
+    uart_callback_set(uart_dev, uart_callback, NULL);
+    rxbuf = rxbuf1;
+    uart_rx_enable(uart_dev, rxbuf, BUF_SIZE, 10000);
+
+    k_thread_create(
+        &thread_data, stack_area,
+        K_THREAD_STACK_SIZEOF(stack_area),
+        testUart,
+        NULL, NULL, NULL,
+        THREAD_PRIORITY, 0, K_NO_WAIT
+    );
+    k_thread_name_set(&thread_data, "test_uart");
 }
