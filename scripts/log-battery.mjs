14a3f1b3d7a76692e12583ab66dd208c40ffc76d
László Monda <laci@monda.hu>
2024-04-24 19:20:21 +0200

    Log STAT transitions.

--- scripts/log-battery.mjs
+++ scripts/log-battery.mjs
@@ -13,9 +13,14 @@ const port = new SerialPort({path: devFilename, baudRate: 115200})
 
 const now = moment();
 const dateString = now.format('YYYY-MM-DD_HH:mm:ss');
+
 const csvFilename = `battery-stats/${dateString}_${half}.csv`;
 const csvFile = fs.createWriteStream(csvFilename);
 csvFile.write('minutes,charger_en,stat,ts_raw,ts_mV,vbat_raw,vbat_mV\n');
+
+const statFilename = `stat-changes/${dateString}_${half}.csv`;
+const statFile = fs.createWriteStream(statFilename);
+statFile.write('minutes,charger_en,stat,ts_raw,ts_mV,vbat_raw,vbat_mV,statChangedTo\n');
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
@@ -23,12 +28,15 @@ port.on('error', function(err) {
   console.log('Error: ', err.message);
 })
 
+let parsedData = {};
 parser.on('data', (data) => {
+  const elapsedTime = Math.round((Date.now() - startTime) / 1000 / 60);
   console.log('Read data: ', data.toString());
-  const regex = /CHARGER_EN: (\d) \| STAT: (\d) \| TS: (\d+) = (\d+) mV \| VBAT: (\d+) = (\d+) mV/;
-  const match = data.match(regex);
+
+  const chargerRegex = /CHARGER_EN: (\d) \| STAT: (\d) \| TS: (\d+) = (\d+) mV \| VBAT: (\d+) = (\d+) mV/;
+  let match = data.match(chargerRegex);
   if (match) {
-    const parsedData = {
+    parsedData = {
       charger_en: parseInt(match[1], 10),
       stat: parseInt(match[2], 10),
       ts: {
@@ -41,9 +49,15 @@ parser.on('data', (data) => {
       }
     };
     console.log(parsedData);
-
-    const elapsedTime = Math.round((Date.now() - startTime) / 1000 / 60);
     csvFile.write(`${elapsedTime},${parsedData.charger_en},${parsedData.stat},${parsedData.ts.raw},${parsedData.ts.mV},${parsedData.vbat.raw},${parsedData.vbat.mV}\n`);
+  }
+
+  const statRegex = /STAT changed to (\d)/;
+  match = data.match(statRegex);
+  if (match) {
+    const statChangedTo = parseInt(match[1], 10);
+    console.log(match[0], statChangedTo);
+    statFile.write(`${elapsedTime},${parsedData.charger_en},${parsedData.stat},${parsedData.ts.raw},${parsedData.ts.mV},${parsedData.vbat.raw},${parsedData.vbat.mV},${statChangedTo}\n`);
   }
 });
 
@@ -60,6 +74,8 @@ async function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+sendCommand('uhk charger');
+sendCommand('uhk statlog 1');
 while (true) {
   sendCommand('uhk charger');
   await sleep(60000);
