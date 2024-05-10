5086d2588d120fb2c5c897de5ee42c3b62e48799
László Monda <laci@monda.hu>
2024-05-10 21:40:17 +0200

    Make the dongle only scan the right half for now.

--- device/src/bt_scan.c
+++ device/src/bt_scan.c
@@ -20,7 +20,6 @@ static void scan_connecting(struct bt_scan_device_info *device_info, struct bt_c
 BT_SCAN_CB_INIT(scan_cb, scan_filter_match, NULL, scan_connecting_error, scan_connecting);
 
 int scan_init(void) {
-    int err;
     struct bt_scan_init_param scan_init = {
         .connect_if_match = 1,
     };
@@ -28,21 +27,21 @@ int scan_init(void) {
     bt_scan_init(&scan_init);
     bt_scan_cb_register(&scan_cb);
 
-#if DEVICE_IS_UHK80_RIGHT || DEVICE_IS_UHK_DONGLE
-    err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_ADDR, &Peers[PeerIdLeft].addr);
-    if (err) {
-        printk("Scanning filters cannot be set (err %d)\n", err);
-        return err;
+    int err;
+    if (DEVICE_IS_UHK80_RIGHT) {
+        err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_ADDR, &Peers[PeerIdLeft].addr);
+        if (err) {
+            printk("Scanning filters cannot be set (err %d)\n", err);
+            return err;
+        }
     }
-#endif
-
-#if DEVICE_IS_UHK_DONGLE
-    err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_ADDR, &Peers[PeerIdRight].addr);
-    if (err) {
-        printk("Scanning filters cannot be set (err %d)\n", err);
-        return err;
+    if (DEVICE_IS_UHK_DONGLE) {
+        err = bt_scan_filter_add(BT_SCAN_FILTER_TYPE_ADDR, &Peers[PeerIdRight].addr);
+        if (err) {
+            printk("Scanning filters cannot be set (err %d)\n", err);
+            return err;
+        }
     }
-#endif
 
     err = bt_scan_filter_enable(BT_SCAN_ADDR_FILTER, false);
     if (err) {
