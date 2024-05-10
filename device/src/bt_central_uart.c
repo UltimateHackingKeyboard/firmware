c8081468c1e1882e2f0078510604e6a99975ae6b
László Monda <laci@monda.hu>
2024-05-11 00:53:07 +0200

    Use same-line curlies.

--- device/src/bt_central_uart.c
+++ device/src/bt_central_uart.c
@@ -13,16 +13,14 @@
 
 static struct bt_nus_client nus_client;
 
-static void ble_data_sent(struct bt_nus_client *nus, uint8_t err, const uint8_t *const data, uint16_t len)
-{
+static void ble_data_sent(struct bt_nus_client *nus, uint8_t err, const uint8_t *const data, uint16_t len) {
     printk("NUS data sent to %s: %i\n", GetPeerStringByConn(nus->conn), len);
     if (err) {
         printk("ATT error code: 0x%02X\n", err);
     }
 }
 
-static uint8_t ble_data_received(struct bt_nus_client *nus, const uint8_t *data, uint16_t len)
-{
+static uint8_t ble_data_received(struct bt_nus_client *nus, const uint8_t *data, uint16_t len) {
 #if DEVICE_IS_UHK80_RIGHT
     printk("Received data: %i %i %i %i %i %i %i %i\n", data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
     for (uint8_t keyId = 0; keyId < MAX_KEY_COUNT_PER_MODULE; keyId++) {
@@ -32,8 +30,7 @@ static uint8_t ble_data_received(struct bt_nus_client *nus, const uint8_t *data,
     return BT_GATT_ITER_CONTINUE;
 }
 
-static void discovery_complete(struct bt_gatt_dm *dm, void *context)
-{
+static void discovery_complete(struct bt_gatt_dm *dm, void *context) {
     struct bt_nus_client *nus = context;
     printk("Service discovery completed\n");
 
@@ -45,13 +42,11 @@ static void discovery_complete(struct bt_gatt_dm *dm, void *context)
     bt_gatt_dm_data_release(dm);
 }
 
-static void discovery_service_not_found(struct bt_conn *conn, void *context)
-{
+static void discovery_service_not_found(struct bt_conn *conn, void *context) {
     printk("Service not found\n");
 }
 
-static void discovery_error(struct bt_conn *conn, int err, void *context)
-{
+static void discovery_error(struct bt_conn *conn, int err, void *context) {
     printk("Error while discovering GATT database: (%d)\n", err);
 }
 
@@ -61,16 +56,14 @@ struct bt_gatt_dm_cb discovery_cb = {
     .error_found       = discovery_error,
 };
 
-void gatt_discover(struct bt_conn *conn)
-{
+void gatt_discover(struct bt_conn *conn) {
     int err = bt_gatt_dm_start(conn, BT_UUID_NUS_SERVICE, &discovery_cb, &nus_client);
     if (err) {
         printk("could not start the discovery procedure, error code: %d\n", err);
     }
 }
 
-static void exchange_func(struct bt_conn *conn, uint8_t err, struct bt_gatt_exchange_params *params)
-{
+static void exchange_func(struct bt_conn *conn, uint8_t err, struct bt_gatt_exchange_params *params) {
     if (!err) {
         printk("MTU exchange done with %s\n", GetPeerStringByConn(conn));
     } else {
@@ -78,8 +71,7 @@ static void exchange_func(struct bt_conn *conn, uint8_t err, struct bt_gatt_exch
     }
 }
 
-void SetupCentralConnection(struct bt_conn *conn)
-{
+void SetupCentralConnection(struct bt_conn *conn) {
     static struct bt_gatt_exchange_params exchange_params;
 
     exchange_params.func = exchange_func;
@@ -101,8 +93,7 @@ void SetupCentralConnection(struct bt_conn *conn)
     }
 }
 
-static int nus_client_init(void)
-{
+static int nus_client_init(void) {
     struct bt_nus_client_init_param init = {
         .cb = {
             .received = ble_data_received,
@@ -120,8 +111,7 @@ static int nus_client_init(void)
     return err;
 }
 
-void InitCentralUart(void)
-{
+void InitCentralUart(void) {
     int err = scan_init();
     if (err != 0) {
         printk("scan_init failed (err %d)\n", err);
@@ -143,8 +133,7 @@ void InitCentralUart(void)
     printk("Scanning successfully started\n");
 }
 
-void SendCentralUart(const uint8_t *data, uint16_t len)
-{
+void SendCentralUart(const uint8_t *data, uint16_t len) {
     int err = bt_nus_client_send(&nus_client, data, len);
     if (err) {
         printk("Failed to send data over BLE connection (err %d)\n", err);
