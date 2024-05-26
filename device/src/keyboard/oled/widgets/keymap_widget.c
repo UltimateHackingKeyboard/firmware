f23f7874c430ac123b4295a2f782dbb0882a7243
Karel Tuček <kareltucek@centrum.cz>
2024-05-26 10:18:38 +0200

    Fix string for FTY keymap.

--- device/src/keyboard/oled/widgets/keymap_widget.c
+++ device/src/keyboard/oled/widgets/keymap_widget.c
@@ -12,13 +12,21 @@
 
 static bool needsUpdate = true;
 
+static string_segment_t getKeymapString() {
+    if (strcmp(AllKeymaps[CurrentKeymapIndex].abbreviation, "FTY") == 0) {
+        return (string_segment_t){ .start = "Factory default", .end = NULL };
+    } else {
+        return GetKeymapName(CurrentKeymapIndex);
+    }
+}
+
 static void drawKeymap(widget_t* self, framebuffer_t* buffer)
 {
     if (self->dirty || needsUpdate) {
         self->dirty = false;
         needsUpdate = false;
         Framebuffer_Clear(self, buffer);
-        string_segment_t text = GetKeymapName(CurrentKeymapIndex);
+        string_segment_t text = getKeymapString();
         Framebuffer_DrawTextAnchored(self, buffer, AnchorType_Center, AnchorType_Center, &JetBrainsMono16, text.start, text.end);
     }
 }
