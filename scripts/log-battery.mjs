c48ef0e46d5485acfee7fe3d06e1381bf38530c5
László Monda <laci@monda.hu>
2024-05-27 21:50:07 +0200

    Fix log-battery.mjs now that TS has been removed. Add documentation to script header. Add 99-usb-serial.rules

--- scripts/log-battery.mjs
+++ scripts/log-battery.mjs
@@ -1,4 +1,16 @@
 #!/bin/env node
+
+// This script logs battery stats and STAT pin transitions to CSV files.
+// Before running it, mkdir battery-stats and stat-changes directories.
+// Use `npm i` to install the required dependencies.
+//
+// cp 99-usb-serial.rules /etc/udev/rules.d/
+// Modify the udev file to reflect the serial numbers of your adapters.
+// Run sudo udevadm control --reload-rules
+// Afterward, your serial to USB adapters should be available at /dev/ttyUSB-{left,right,dongle}
+//
+// Finally, run the script with log-battery.mjs left|right|dongle
+
 import fs from 'fs';
 import moment from 'moment';
 import {SerialPort} from 'serialport';
@@ -16,11 +28,11 @@ const dateString = now.format('YYYY-MM-DD_HH:mm:ss');
 
 const csvFilename = `battery-stats/${dateString}_${half}.csv`;
 const csvFile = fs.createWriteStream(csvFilename);
-csvFile.write('minutes,charger_en,stat,ts_raw,ts_mV,vbat_raw,vbat_mV\n');
+csvFile.write('minutes,charger_en,stat,vbat_raw,vbat_mV\n');
 
 const statFilename = `stat-changes/${dateString}_${half}.csv`;
 const statFile = fs.createWriteStream(statFilename);
-statFile.write('minutes,charger_en,stat,ts_raw,ts_mV,vbat_raw,vbat_mV,statChangedTo\n');
+statFile.write('minutes,charger_en,stat,vbat_raw,vbat_mV,statChangedTo\n');
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
@@ -33,23 +45,19 @@ parser.on('data', (data) => {
   const elapsedTime = Math.round((Date.now() - startTime) / 1000 / 60);
   console.log('Read data: ', data.toString());
 
-  const chargerRegex = /CHARGER_EN: (\d) \| STAT: (\d) \| TS: (\d+) = (\d+) mV \| VBAT: (\d+) = (\d+) mV/;
+  const chargerRegex = /CHARGER_EN: (\d) \| STAT: (\d) \| VBAT: (\d+) = (\d+) mV/;
   let match = data.match(chargerRegex);
   if (match) {
     parsedData = {
       charger_en: parseInt(match[1], 10),
       stat: parseInt(match[2], 10),
-      ts: {
+      vbat: {
         raw: parseInt(match[3], 10),
         mV: parseInt(match[4], 10)
-      },
-      vbat: {
-        raw: parseInt(match[5], 10),
-        mV: parseInt(match[6], 10)
       }
     };
     console.log(parsedData);
-    csvFile.write(`${elapsedTime},${parsedData.charger_en},${parsedData.stat},${parsedData.ts.raw},${parsedData.ts.mV},${parsedData.vbat.raw},${parsedData.vbat.mV}\n`);
+    csvFile.write(`${elapsedTime},${parsedData.charger_en},${parsedData.stat},${parsedData.vbat.raw},${parsedData.vbat.mV}\n`);
   }
 
   const statRegex = /STAT changed to (\d)/;
@@ -57,7 +65,7 @@ parser.on('data', (data) => {
   if (match) {
     const statChangedTo = parseInt(match[1], 10);
     console.log(match[0], statChangedTo);
-    statFile.write(`${elapsedTime},${parsedData.charger_en},${parsedData.stat},${parsedData.ts.raw},${parsedData.ts.mV},${parsedData.vbat.raw},${parsedData.vbat.mV},${statChangedTo}\n`);
+    statFile.write(`${elapsedTime},${parsedData.charger_en},${parsedData.stat},${parsedData.vbat.raw},${parsedData.vbat.mV},${statChangedTo}\n`);
   }
 });
 
