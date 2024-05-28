1f4e34e1ca157ba44c4046a8eb3edb30efaf6f01
Karel Tuček <kareltucek@centrum.cz>
2024-05-28 22:27:05 +0200

    Script improvements.

--- scripts/log-battery.mjs
+++ scripts/log-battery.mjs
@@ -4,12 +4,15 @@
 // Before running it, mkdir battery-stats and stat-changes directories.
 // Use `npm i` to install the required dependencies.
 //
-// cp 99-usb-serial.rules /etc/udev/rules.d/
-// Modify the udev file to reflect the serial numbers of your adapters.
-// Run sudo udevadm control --reload-rules
-// Afterward, your serial to USB adapters should be available at /dev/ttyUSB-{left,right,dongle}
+// (Optional) create fixed dev points:
+//
+//   cp 99-usb-serial.rules /etc/udev/rules.d/
+//   Modify the udev file to reflect the serial numbers of your adapters.
+//   Run sudo udevadm control --reload-rules
+//   Afterward, your serial to USB adapters should be available at /dev/ttyUSB-{left,right,dongle}
 //
 // Finally, run the script with log-battery.mjs left|right|dongle
+// Or to use raw /dev/ttyUSBx, run just log-battery.mjs 0
 
 import fs from 'fs';
 import moment from 'moment';
@@ -18,9 +21,9 @@ import {ReadlineParser} from '@serialport/parser-readline';
 const startTime = Date.now();
 
 const args = process.argv.slice(2);
-const half = args[0];
+const half = /^[a-zA-Z]/.test(args[0]) ? `-${args[0]}` : args[0];
 
-const devFilename = `/dev/ttyUSB-${half}`;
+const devFilename = `/dev/ttyUSB${half}`;
 const port = new SerialPort({path: devFilename, baudRate: 115200})
 
 const now = moment();
