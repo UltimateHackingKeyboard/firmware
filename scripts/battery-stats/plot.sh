040b3021ec31ce4c16d0cfc21d4987d580c7f3c9
Karel Tuček <kareltucek@centrum.cz>
2024-06-03 00:54:21 +0200

    Plot battery graph on two axes...

--- scripts/battery-stats/plot.sh
+++ scripts/battery-stats/plot.sh
@@ -17,22 +17,26 @@ gnuplot -persist <<-EOF
     set key autotitle columnheader
     set xlabel 'X-axis'
     set ylabel 'Y-axis'
+    set y2label 'Y2-axis'
     set grid
+    set grid ytics lt 1 lw 1 lc rgb "#bbbbbb"
+    set ytics 100
+    set y2tics
+    set mxtics 5
+    set mytics 5
     set terminal wxt size 1200,800
 
     # Set the y range for the first y axis
-    set yrange [0:5]
+    set yrange [2000:4300]
 
     # Set up the second y axis with range
-    set y2tics
-    set y2label 'Y2-axis'
-    set y2range [2000:4300]
+    set y2range [0:5]
 
     # Determine the number of columns in the CSV file
     n = system(sprintf("awk -F',' 'NR==1{print NF}' %s", '$CSV_FILE'))
 
-    # Plot the first and second columns on y axis and subsequent columns on y2 axis
-    plot '$CSV_FILE' using 1:2 title columnheader(2) with linespoints axis x1y1, \
-         '$CSV_FILE' using 1:3 title columnheader(3) with linespoints axis x1y1, \
-         for [i=4:n] '$CSV_FILE' using 1:i title columnheader(i) with linespoints axis x1y2
+    # Plot the first and second columns on the y2 axis (second y axis)
+    plot '$CSV_FILE' using 1:2 title columnheader(2) with linespoints axis x1y2, \
+         '$CSV_FILE' using 1:3 title columnheader(3) with linespoints axis x1y2, \
+         for [i=4:n] '$CSV_FILE' using 1:i title columnheader(i) with linespoints axis x1y1
 EOF
