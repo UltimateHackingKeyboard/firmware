e723ea7080d06cf5975fdc0fea17ff8990044e85
László Monda <laci@monda.hu>
2024-06-12 20:17:19 +0200

    Manage IsStorageBusy in Flash_LaunchTransfer()

--- device/src/flash.c
+++ device/src/flash.c
@@ -8,12 +8,14 @@ uint8_t Flash_LaunchTransfer(storage_operation_t operation, config_buffer_id_t c
     const struct flash_area *configArea = configBufferId == ConfigBufferId_HardwareConfig ? hardwareConfigArea : userConfigArea;
     size_t configSize = configBufferId == ConfigBufferId_HardwareConfig ? HARDWARE_CONFIG_SIZE : USER_CONFIG_SIZE;
 
+    IsStorageBusy = true;
     if (operation == StorageOperation_Read) {
         flash_area_read(configArea, 0, ConfigBufferIdToConfigBuffer(configBufferId)->buffer, configSize);
     } else if (operation == StorageOperation_Write) {
         flash_area_erase(configArea, 0, configSize);
         flash_area_write(configArea, 0, ConfigBufferIdToConfigBuffer(configBufferId)->buffer, configSize);
     }
+    IsStorageBusy = false;
 
     return 0;
 }
