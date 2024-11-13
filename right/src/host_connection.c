d2e608d7cca5080410d1ee9b4ed1d53ee672cc7d
Karel Tuček <kareltucek@centrum.cz>
2024-11-13 23:07:25 +0100

    Fix some pairing-related issues.

--- right/src/host_connection.c
+++ right/src/host_connection.c
@@ -1,6 +1,7 @@
 #include "host_connection.h"
 
 #ifdef __ZEPHYR__
+#include "bt_conn.h"
 
 host_connection_t HostConnections[HOST_CONNECTION_COUNT_MAX] = {};
 
@@ -21,6 +22,13 @@ bool HostConnections_IsKnownBleAddress(const bt_addr_le_t *address) {
                 break;
         }
     }
+
+    for (int peerIdx = 0; peerIdx < PeerIdHid; peerIdx++) {
+        if (bt_addr_le_cmp(address, &Peers[peerIdx].addr) == 0) {
+            return true;
+        }
+    }
+
     return false;
 }
 
