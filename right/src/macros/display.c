2574ade3be69f43d791ef944914fb89ea14d6822
Karel Tucek <kareltucek@centrum.cz>
2025-04-11 16:17:41 +0200

    Fix setLedTxt dry run validation warnings.

--- right/src/macros/display.c
+++ right/src/macros/display.c
@@ -218,7 +218,7 @@ macro_result_t Macros_ProcessSetLedTxtCommand(parser_context_t* ctx)
         processList(ctx, false, time);
         return MacroResult_Finished;
     } else {
-        if (!delayIsInProgress) {
+        if (!delayIsInProgress || Macros_DryRun) {
             processList(ctx, true, time);
         }
         return res;
