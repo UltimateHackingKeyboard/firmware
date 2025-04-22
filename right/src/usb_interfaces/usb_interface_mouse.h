0858dc911c315ef23488af7d98a8a3229f528f15
Benedek Kupper <kupper.benedek@gmail.com>
2025-04-22 23:49:07 +0200

    uhk60: add report ID to mouse reports to dodge the Linux bug
    The Linux kernel sends 0 length SetReport request for the high-res multiplier
    feature, when the report descriptor doesn't use report IDs.
    Define an ID, which also must be used for the input report.

--- right/src/usb_interfaces/usb_interface_mouse.h
+++ right/src/usb_interfaces/usb_interface_mouse.h
@@ -5,6 +5,7 @@
 
     #include "usb_api.h"
     // #include "usb_descriptors/usb_descriptor_device.h"
+    #include "usb_descriptors/usb_descriptor_mouse_report.h"
 
 // Macros:
 
@@ -18,7 +19,6 @@
     #define USB_MOUSE_INTERRUPT_IN_INTERVAL 1
 
     #define USB_MOUSE_REPORT_LENGTH (sizeof(usb_mouse_report_t))
-    #define USB_MOUSE_FEAT_REPORT_LENGTH 1
 
 // Typedefs:
 
@@ -29,6 +29,9 @@
     // reports to the boot protocol format when the host has set boot protocol
     // mode.
     typedef struct {
+#ifdef USB_MOUSE_REPORT_ID
+        uint8_t id;
+#endif
         uint32_t buttons : 24;
         int16_t x;
         int16_t y;
