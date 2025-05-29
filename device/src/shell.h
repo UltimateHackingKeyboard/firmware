c1389944719671d55aed71f985a51e0803b225d1
Karel Tucek <kareltucek@centrum.cz>
2025-05-29 11:13:37 +0200

    Allow executing zephyr commands from macros.

--- device/src/shell.h
+++ device/src/shell.h
@@ -21,6 +21,6 @@ extern shell_t Shell;
 // Functions:
 
 extern void InitShell(void);
-extern void Shell_Execute(const char *cmd);
+extern void Shell_Execute(const char *cmd, const char *source);
 
 #endif // SHELL_H__
