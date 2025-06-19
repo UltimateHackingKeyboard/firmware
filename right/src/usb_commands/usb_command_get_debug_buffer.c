11c587d1406e1d01b42013c7600470493c37ee1e
Benedek Kupper <kupper.benedek@gmail.com>
2025-06-19 09:34:12 +0200

    right: eliminate unnecessary debug buffer

--- right/src/usb_commands/usb_command_get_debug_buffer.c
+++ right/src/usb_commands/usb_command_get_debug_buffer.c
@@ -22,57 +22,20 @@ uint8_t DebugBuffer[USB_GENERIC_HID_IN_BUFFER_LENGTH];
 void UsbCommand_GetDebugBuffer(const uint8_t *GenericHidOutBuffer, uint8_t *GenericHidInBuffer)
 {
 #ifndef __ZEPHYR__
-    SetDebugBufferUint32(1, I2C_Watchdog);
-    SetDebugBufferUint32(5, I2cSlaveScheduler_Counter);
-    SetDebugBufferUint32(9, I2cWatchdog_WatchCounter);
-    SetDebugBufferUint32(13, I2cWatchdog_RecoveryCounter);
-    SetDebugBufferUint32(17, MatrixScanCounter);
-    SetDebugBufferUint32(21, UsbReportUpdateCounter);
+    SetBufferUint32(GenericHidInBuffer, 1, I2C_Watchdog);
+    SetBufferUint32(GenericHidInBuffer, 5, I2cSlaveScheduler_Counter);
+    SetBufferUint32(GenericHidInBuffer, 9, I2cWatchdog_WatchCounter);
+    SetBufferUint32(GenericHidInBuffer, 13, I2cWatchdog_RecoveryCounter);
+    SetBufferUint32(GenericHidInBuffer, 17, MatrixScanCounter);
+    SetBufferUint32(GenericHidInBuffer, 21, UsbReportUpdateCounter);
 #endif
-    SetDebugBufferUint32(25, CurrentTime);
+    SetBufferUint32(GenericHidInBuffer, 25, CurrentTime);
 #ifndef __ZEPHYR__
-    SetDebugBufferUint32(29, UsbGenericHidActionCounter);
-    SetDebugBufferUint32(33, UsbBasicKeyboardActionCounter);
-    SetDebugBufferUint32(37, UsbMediaKeyboardActionCounter);
-    SetDebugBufferUint32(41, UsbSystemKeyboardActionCounter);
-    SetDebugBufferUint32(45, UsbMouseActionCounter);
-    SetDebugBufferUint32(49, UsbGamepadActionCounter);
+    SetBufferUint32(GenericHidInBuffer, 29, UsbGenericHidActionCounter);
+    SetBufferUint32(GenericHidInBuffer, 33, UsbBasicKeyboardActionCounter);
+    SetBufferUint32(GenericHidInBuffer, 37, UsbMediaKeyboardActionCounter);
+    SetBufferUint32(GenericHidInBuffer, 41, UsbSystemKeyboardActionCounter);
+    SetBufferUint32(GenericHidInBuffer, 45, UsbMouseActionCounter);
+    SetBufferUint32(GenericHidInBuffer, 49, UsbGamepadActionCounter);
 #endif
-
-    memcpy(GenericHidInBuffer, DebugBuffer, USB_GENERIC_HID_IN_BUFFER_LENGTH);
-}
-
-void SetDebugBufferUint8(uint32_t offset, uint8_t value)
-{
-    SetBufferUint8(DebugBuffer, offset, value);
-}
-
-void SetDebugBufferUint16(uint32_t offset, uint16_t value)
-{
-    SetBufferUint16(DebugBuffer, offset, value);
-}
-
-void SetDebugBufferUint32(uint32_t offset, uint32_t value)
-{
-    SetBufferUint32(DebugBuffer, offset, value);
-}
-
-void SetDebugBufferInt8(uint32_t offset, int8_t value)
-{
-    SetBufferInt8(DebugBuffer, offset, value);
-}
-
-void SetDebugBufferInt16(uint32_t offset, int16_t value)
-{
-    SetBufferInt16(DebugBuffer, offset, value);
-}
-
-void SetDebugBufferInt32(uint32_t offset, int32_t value)
-{
-    SetBufferInt32(DebugBuffer, offset, value);
-}
-
-void SetDebugBufferFloat(uint32_t offset, float value)
-{
-    *(float*)(DebugBuffer + offset) = value;
 }
