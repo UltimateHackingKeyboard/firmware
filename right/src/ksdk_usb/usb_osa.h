2134339aaa23b3570e51b224385c812e501aeb76
Benedek Kupper <kupper.benedek@gmail.com>
2025-06-20 15:33:39 +0200

    right: fix USB stack update related build issues

---
+++ right/src/ksdk_usb/usb_osa.h
@@ -0,0 +1,10 @@
+#ifndef __USB_OSA_H__
+#define __USB_OSA_H__
+
+#include "fsl_rtos_abstraction.h"
+
+#define USB_OSA_SR_ALLOC() OSA_SR_ALLOC()
+#define USB_OSA_ENTER_CRITICAL() OSA_ENTER_CRITICAL()
+#define USB_OSA_EXIT_CRITICAL() OSA_EXIT_CRITICAL()
+
+#endif
