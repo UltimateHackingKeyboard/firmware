fce1788c3c181d890d88b6304b80a6f63d3d9c19
Benedek Kupper <kupper.benedek@gmail.com>
2025-06-26 20:56:37 +0200

    module: update GPIO driver API

--- shared/module/init_peripherals.c
+++ shared/module/init_peripherals.c
@@ -75,7 +75,7 @@ void InitLedDriver(void)
     CLOCK_EnableClock(LED_DRIVER_SDB_CLOCK);
     PORT_SetPinMux(LED_DRIVER_SDB_PORT, LED_DRIVER_SDB_PIN, kPORT_MuxAsGpio);
     GPIO_PinInit(LED_DRIVER_SDB_GPIO, LED_DRIVER_SDB_PIN, &(gpio_pin_config_t){kGPIO_DigitalOutput, 0});
-    GPIO_WritePinOutput(LED_DRIVER_SDB_GPIO, LED_DRIVER_SDB_PIN, 1);
+    GPIO_PinWrite(LED_DRIVER_SDB_GPIO, LED_DRIVER_SDB_PIN, 1);
 }
 
 void InitPeripherals(void)
