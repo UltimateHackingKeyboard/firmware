f59dc2916156ff53ea29c409934bf2c33dfdd4c7
Karel Tucek <kareltucek@centrum.cz>
2025-06-30 10:54:36 +0200

    Print reset reason registers.

---
+++ right/src/trace_reasons.h
@@ -0,0 +1,18 @@
+#ifndef __TRACE_REASONS_H__
+#define __TRACE_REASONS_H__
+
+// Includes:
+
+    #include <inttypes.h>
+    #include <stdbool.h>
+    #include "debug.h"
+    #include "logger.h"
+
+// Functions:
+
+#ifndef __ZEPHYR__
+    void Trace_PrintUhk60ReasonRegisters(device_id_t targetDeviceId, log_target_t targetInterface);
+#endif
+
+#endif
+
