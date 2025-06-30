f935d049c076b0a36221ea1f84f3908a944168ba
Karel Tucek <kareltucek@centrum.cz>
2025-06-30 15:01:23 +0200

    Fix voltage averaging quirks when (dis)connecting battery.

--- device/src/keyboard/battery_window_calculator.c
+++ device/src/keyboard/battery_window_calculator.c
@@ -1,6 +1,9 @@
 #include "battery_window_calculator.h"
 #include "timer.h"
 #include <inttypes.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(Battery);
 
 uint32_t lastMeasurement = 0;
 
@@ -35,18 +38,27 @@ static void addNewRecord(uint16_t voltage) {
 }
 
 uint16_t BatteryCalculator_CalculateWindowAverageVoltage(uint16_t voltage) {
-    if (BATTERY_CALCULATOR_AVERAGE_ENABLED) {
+    if (BATTERY_CALCULATOR_AVERAGE_ENABLED && voltage != 0) {
         // printk("BatteryCalculator_CalculateWindowAverageVoltage (%d) of (%d):", voltage, count);
         // for (uint8_t i = 0; i < count; i++) {
         //     printk(" %d", values[i]);
         // }
         // printk(" sums to %d\n", sum);
+        uint16_t res;
         if (shouldAddRecord(voltage)) {
             addNewRecord(voltage);
-            return sum / count;
+            res = sum / count;
         } else {
-            return (sum + voltage) / (count + 1);
+            res =  (sum + voltage) / (count + 1);
         }
+        if ((voltage > res && voltage - res > 200) || (res < voltage && voltage - res > 200)) {
+            LOG_WRN("Battery average flew away, resetting window to %d\n", voltage);
+            addNewRecord(voltage);
+            count = 1;
+            sum = voltage;
+            res = voltage;
+        }
+        return res;
     } else {
         return voltage;
     }
