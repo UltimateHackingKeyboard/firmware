b3e5f90cd2759ec51be4c4b6efed3acdfe38ebb5
Benedek Kupper <kupper.benedek@gmail.com>
2025-08-12 23:25:50 +0200

    add support for sysbuild for nRF builds

--- scripts/make-release.mjs
+++ scripts/make-release.mjs
@@ -37,7 +37,6 @@ function build(buildTarget, step) {
     } else if (buildTarget.platform === 'nordic') {
         shell.exec(`ZEPHYR_TOOLCHAIN_VARIANT=zephyr west config manifest.file west_nrfsdk.yml && \
             west build --build-dir ${projectName}/build/${buildTarget.name} ${projectName} \
-            --no-sysbuild \
             ${pristineFlag} \
             -- \
             --preset ${buildTarget.name} \
