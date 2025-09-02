56d35a3091a10b294ee7ac2a9f8176978225d502
Benedek Kupper <kupper.benedek@gmail.com>
2025-09-02 14:25:18 +0200

    fix compilation error

--- device/src/keyboard/uart.c
+++ device/src/keyboard/uart.c
@@ -321,7 +321,6 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
 
     case UART_RX_STOPPED:
         LogU("UART_RX_STOPPED, because %d\n", evt->data.rx_stop.reason  );
-        CurrentTime = k_uptime_get();
         EventScheduler_Schedule(Timer_GetCurrentTime() + 1000, EventSchedulerEvent_ReenableUart, "reenable uart");
         break;
     }
