640c1a4fd9442a86f0d43c7ad4392dada8f67fca
Karel Tucek <kareltucek@centrum.cz>
2025-11-17 12:49:19 +0100

    Enable reset button pullup before enabling the interrupt.

--- right/src/peripherals/reset_button.c
+++ right/src/peripherals/reset_button.c
@@ -40,11 +40,11 @@ void InitResetButton(void)
 {
     CLOCK_EnableClock(RESET_BUTTON_CLOCK);
     PORT_SetPinInterruptConfig(RESET_BUTTON_PORT, RESET_BUTTON_PIN, kPORT_InterruptFallingEdge);
-    EnableIRQ(RESET_BUTTON_IRQ);
     PORT_SetPinConfig(RESET_BUTTON_PORT, RESET_BUTTON_PIN,
                       &(port_pin_config_t){.pullSelect=kPORT_PullUp, .mux=kPORT_MuxAsGpio});
     SDK_DelayAtLeastUs(10, SystemCoreClock);
     factoryResetModeEnabled = RESET_BUTTON_IS_PRESSED;
+    EnableIRQ(RESET_BUTTON_IRQ);
 }
 
 bool IsFactoryResetModeEnabled(void)
