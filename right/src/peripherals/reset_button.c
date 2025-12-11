134df820ef35f071530317562394483e5bc5bf34
Karel Tucek <kareltucek@centrum.cz>
2025-12-11 17:30:00 +0100

    On spurious resets, just reboot.

--- right/src/peripherals/reset_button.c
+++ right/src/peripherals/reset_button.c
@@ -13,25 +13,13 @@
 
 void RESET_BUTTON_IRQ_HANDLER(void)
 {
-    static uint8_t count = 0;
-
-    if (count++ > 20) {
-        DisableIRQ(RESET_BUTTON_IRQ);
-        if (Cfg.DevMode) {
-            Macros_ReportErrorPrintf(NULL, "Uptime: %d. Looks like spurious factory button activation. Disabling the reset button.", Timer_GetCurrentTime());
-        }
-        return;
+    if (Cfg.DevMode) {
+        Macros_ReportErrorPrintf(NULL, "Uptime: %d. Rebooting because button got activated. Reporting as a crash because dev mode is on.", Timer_GetCurrentTime());
+        StateWormhole.wasReboot = false;
+    } else {
+        StateWormhole.wasReboot = true;
     }
 
-    // We are getting spurious activations, so check that it is pressed for at least some 20ms straight
-    for (volatile uint32_t i = 0; i < 300*1000; i++) {
-    }
-
-    if (!RESET_BUTTON_IS_PRESSED) {
-        return;
-    }
-
-    StateWormhole.wasReboot = true;
     Wormhole.magicNumber = WORMHOLE_MAGIC_NUMBER;
     Wormhole.enumerationMode = EnumerationMode_NormalKeyboard;
     Trace_Printc("RstButton");
