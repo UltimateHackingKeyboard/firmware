562c495e22463f60be8504225bf33122529501e3
Karel Tucek <kareltucek@centrum.cz>
2025-12-31 23:39:28 +0100

    Add string var support. Add $uhk.name variable.

--- right/src/macros/string_reader.h
+++ right/src/macros/string_reader.h
@@ -26,6 +26,7 @@
 // Functions:
 
     char Macros_ConsumeCharOfString(parser_context_t* ctx, uint16_t* stringOffset, uint16_t* index, uint16_t* subIndex);
+    bool Macros_CompareStringRefs(string_ref_t ref1, string_ref_t ref2);
     bool Macros_CompareStringToken(parser_context_t* ctx, string_segment_t str);
     void Macros_ConsumeStringToken(parser_context_t* ctx);
 
