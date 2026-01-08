5c2ceec0fe505755942a960207e5e4f162843be4
Karel Tucek <kareltucek@centrum.cz>
2026-01-08 16:59:37 +0100

    Fix bluetooth target during switchovers (broken a few commits ago)

--- device/src/keyboard/oled/widgets/widget_store.c
+++ device/src/keyboard/oled/widgets/widget_store.c
@@ -148,19 +148,20 @@ static string_segment_t getTargetText() {
     if ( Macros_DisplayStringsBuffs.host[0] != 0) {
         return (string_segment_t){ .start = Macros_DisplayStringsBuffs.host, .end = NULL };
     } else {
-        string_segment_t currentConnection = getTargetText_(ActiveHostConnectionId, false);
+        bool shortNames = SelectedHostConnectionId != ConnectionId_Invalid;
+        size_t offset = 0;
 
-        string_segment_t selectedConnection = (string_segment_t){ .start = NULL, .end = NULL };
+        string_segment_t currentConnection = getTargetText_(ActiveHostConnectionId, shortNames);
+        offset = snprintf(buffer, sizeof(buffer)-1, "%.*s", SegmentLen(currentConnection), currentConnection.start);
 
         if (SelectedHostConnectionId != ConnectionId_Invalid) {
+            string_segment_t selectedConnection = (string_segment_t){ .start = NULL, .end = NULL };
             selectedConnection = getTargetText_(SelectedHostConnectionId, true);
+            if (selectedConnection.start) {
+                snprintf(buffer+offset, sizeof(buffer)-1-offset, " -> %.*s", SegmentLen(selectedConnection), selectedConnection.start);
+            }
         }
 
-        if (selectedConnection.start) {
-            snprintf(buffer, sizeof(buffer)-1, "%.*s -> %.*s", SegmentLen(currentConnection), currentConnection.start, SegmentLen(selectedConnection), selectedConnection.start);
-        } else {
-            snprintf(buffer, sizeof(buffer)-1, "%.*s", SegmentLen(currentConnection), currentConnection.start);
-        }
         return (string_segment_t){ .start = buffer, .end = NULL };
     }
 };
