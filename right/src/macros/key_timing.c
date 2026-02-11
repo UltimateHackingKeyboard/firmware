a47e06e76eeed0a17013c4106c7c80f6ffe1dc87
Karel Tucek <kareltucek@centrum.cz>
2026-02-11 13:28:41 +0100

    Log key timing into uart too.

--- right/src/macros/key_timing.c
+++ right/src/macros/key_timing.c
@@ -1,54 +1,49 @@
 #include "macros/key_timing.h"
 #include "key_states.h"
-#include "macros/status_buffer.h"
+#include "logger.h"
 #include "secondary_role_driver.h"
 #include "utils.h"
 #include "timer.h"
 
 bool RecordKeyTiming = false;
 
+static log_target_t defaultTarget = LogTarget_Uart | LogTarget_ErrorBuffer;
+
 void KeyTiming_RecordKeystroke(key_state_t *keyState, bool active, uint32_t pressTime, uint32_t activationTime)
 {
     const char* keyAbbreviation = Utils_KeyAbbreviation(keyState);
-    Macros_SetStatusString( active ? "DOWN" : "UP", NULL);
-    Macros_SetStatusChar(' ');
-    Macros_SetStatusString(keyAbbreviation, NULL);
-    Macros_SetStatusNum(pressTime);
-    Macros_SetStatusNum(activationTime);
-    Macros_SetStatusChar('\n');
+    LogTo(DEVICE_ID, defaultTarget, "%s %s %d %d\n",
+        active ? "DOWN" : "UP", keyAbbreviation, pressTime, activationTime);
 }
 
 void KeyTiming_RecordReport(usb_basic_keyboard_report_t* report)
 {
-    Macros_SetStatusNumSpaced(Timer_GetCurrentTime(), false);
-    Utils_PrintReport(" OUT", ActiveUsbBasicKeyboardReport);
+    LogTo(DEVICE_ID, defaultTarget, "%d OUT %s\n",
+        Timer_GetCurrentTime(), Utils_GetUsbReportString(ActiveUsbBasicKeyboardReport));
 }
 
 void KeyTiming_RecordComment(key_state_t* keyState, secondary_role_state_t state, int32_t resolutionLine)
 {
     const char* keyAbbreviation = Utils_KeyAbbreviation(keyState);
 
-    Macros_SetStatusNumSpaced(Timer_GetCurrentTime(), false);
-    Macros_SetStatusChar(' ');
-    Macros_SetStatusString(keyAbbreviation, NULL);
-    Macros_SetStatusChar(' ');
+    char stateChar;
     switch (state) {
     case SecondaryRoleState_Primary:
-        Macros_SetStatusChar('P');
+        stateChar = 'P';
         break;
     case SecondaryRoleState_Secondary:
-        Macros_SetStatusChar('S');
+        stateChar = 'S';
         break;
     case SecondaryRoleState_NoOp:
-        Macros_SetStatusChar('N');
+        stateChar = 'N';
         break;
     case SecondaryRoleState_DontKnowYet:
-        Macros_SetStatusChar('D');
+        stateChar = 'D';
+        break;
+    default:
+        stateChar = '?';
         break;
     }
-    Macros_SetStatusChar(':');
-    Macros_SetStatusNumSpaced(resolutionLine, false);
-    Macros_SetStatusChar('\n');
- 
+    LogTo(DEVICE_ID, defaultTarget, "%d %s %c:%d\n",
+        Timer_GetCurrentTime(), keyAbbreviation, stateChar, resolutionLine);
 }
-
--- right/src/test_suite/tests/test_secondary_roles.c
+++ right/src/test_suite/tests/test_secondary_roles.c
@@ -7,36 +7,21 @@
 // Key u has primary=u, secondary=LS
 // Press u, press i, release i, 20ms, release u -> u i
 static const test_action_t test_secondary_role_positive_safety[] = {
-    // Configure for release-order based triggering with positive safetyMargin
     TEST_SET_CONFIG("secondaryRole.defaultStrategy advanced"),
     TEST_SET_CONFIG("secondaryRole.advanced.timeout 350"),
     TEST_SET_CONFIG("secondaryRole.advanced.timeoutAction secondary"),
     TEST_SET_CONFIG("secondaryRole.advanced.safetyMargin 50"),
     TEST_SET_CONFIG("secondaryRole.advanced.triggeringEvent release"),
-
-    // Set up u as secondary role key: tap=u, hold=LS
     TEST_SET_SECONDARY_ROLE("u", HID_KEYBOARD_SC_U, SecondaryRole_LeftShift),
-    // Set i to output i
     TEST_SET_ACTION("i", "i"),
-
-    // Press u (secondary role key) - nothing happens yet (postponed)
     TEST_PRESS______("u"),
     TEST_DELAY__(20),
-
-    // Press i while holding u - this may trigger secondary depending on config
     TEST_PRESS______("i"),
     TEST_DELAY__(60),
-
-    // Release i first - with release-order triggering and positive margin,
-    // releasing i before u should trigger secondary role
     TEST_RELEASE__U("i"),
     TEST_DELAY__(20),
-
-    // Release u - secondary role (LS) should now activate
     TEST_RELEASE__U("u"),
     TEST_DELAY__(50),
-
-    // Expected: LS activated, i typed with LS modifier, then both released
     TEST_EXPECT__________("u"),
     TEST_EXPECT__________("u i"),
     TEST_EXPECT__________("u "),
@@ -56,7 +41,9 @@ static const test_action_t test_secondary_role_negative_safety[] = {
     TEST_SET_CONFIG("secondaryRole.advanced.timeoutAction secondary"),
     TEST_SET_CONFIG("secondaryRole.advanced.safetyMargin -50"),
     TEST_SET_CONFIG("secondaryRole.advanced.triggeringEvent release"),
+    TEST_DELAY__(50),
     TEST_SET_SECONDARY_ROLE("u", HID_KEYBOARD_SC_U, SecondaryRole_LeftShift),
+    TEST_DELAY__(50),
     TEST_SET_ACTION("i", "i"),
     TEST_PRESS______("u"),
     TEST_DELAY__(20),
