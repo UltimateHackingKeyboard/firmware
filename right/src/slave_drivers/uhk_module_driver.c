7f785b34066e5b9de0772949e1aef8a8a59a9089
Karel Tucek <kareltucek@centrum.cz>
2026-07-27 16:56:38 +0200

    Add a possible race fix.

--- right/src/slave_drivers/uhk_module_driver.c
+++ right/src/slave_drivers/uhk_module_driver.c
@@ -173,7 +173,7 @@ void UhkModuleSlaveDriver_ProcessKeystates(uint8_t uhkModuleDriverId, uhk_module
         }
 
         if (KeyStates[slotId][targetKeyId].hardwareSwitchState != keyStatesBuffer[keyId]) {
-            DEBUG_KEY_LIFE(scan);
+            DEBUG_KEY_LIFE_SCAN(keyStatesBuffer[keyId]);
             KeyStates[slotId][targetKeyId].hardwareSwitchState = keyStatesBuffer[keyId];
             stateChanged = true;
         }
@@ -596,7 +596,18 @@ void UhkModuleSlaveDriver_Disconnect(uint8_t uhkModuleDriverId)
     uint8_t slotId = UhkModuleSlaveDriver_DriverIdToSlotId(uhkModuleDriverId);
 
     if (IS_VALID_MODULE_SLOT(slotId)) {
-        memset(KeyStates[slotId], 0, MAX_KEY_COUNT_PER_MODULE * sizeof(key_state_t));
+        bool stateChanged = false;
+        for (uint8_t keyId = 0; keyId < MAX_KEY_COUNT_PER_MODULE; keyId++) {
+            if (KeyStates[slotId][keyId].hardwareSwitchState) {
+                DEBUG_KEY_LIFE_SCAN(0);
+                KeyStates[slotId][keyId].hardwareSwitchState = false;
+                stateChanged = true;
+            }
+        }
+        if (stateChanged) {
+            EventVector_Set(EventVector_StateMatrix);
+            EventVector_WakeMain();
+        }
     }
 
     EventScheduler_Schedule(Timer_GetCurrentTime() + MODULE_CONNECTION_TIMEOUT, EventSchedulerEvent_ModuleConnectionStatusUpdate, "ModuleConnectionStatusUpdate");
